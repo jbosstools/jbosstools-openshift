ab498c5d5e8254f80b0f13cfbc09510a8fa96c55
Andre Dietisheim <adietish@redhat.com>
2012-01-30 13:43:59 +0000

    [JBIDE-10724] switched from link to hyperlink widget. no displacement is visible any more, the hyperlink is showing up very quickly (whereas link was slow)
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38267 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/propertytable/PropertyValueCellLabelProvider.java
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/propertytable/PropertyValueCellLabelProvider.java
@@ -10,14 +10,16 @@
  ******************************************************************************/
 package org.jboss.tools.openshift.express.internal.ui.propertytable;
 
+import org.eclipse.jface.preference.JFacePreferences;
+import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.viewers.StyledString;
 import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.graphics.TextStyle;
-import org.eclipse.swt.widgets.Link;
 import org.eclipse.swt.widgets.Tree;
+import org.eclipse.ui.forms.widgets.Hyperlink;
 import org.jboss.tools.common.ui.BrowserUtil;
 import org.jboss.tools.openshift.express.internal.ui.OpenShiftUIActivator;
 import org.jboss.tools.openshift.express.internal.ui.utils.TreeUtils;
@@ -39,8 +41,10 @@ public class PropertyValueCellLabelProvider extends AbstractPropertyCellLabelPro
 	}
 
 	protected void createLink(IProperty property, final ViewerCell cell) {
-		Link link = new Link((Tree) cell.getControl(), SWT.NONE);
-		link.setText("<a>" + property.getValue() + "</a>");
+		final Hyperlink link = new Hyperlink((Tree) cell.getControl(), SWT.TRANSPARENT);
+		link.setForeground(JFaceResources.getColorRegistry().get(JFacePreferences.ACTIVE_HYPERLINK_COLOR));
+		link.setUnderlined(true);
+		link.setText(property.getValue());
 		link.setBackground(cell.getBackground());
 		link.addMouseListener(onLinkClicked(property.getValue()));
 		
