c9b07e89d68e02ae7a7f09bb6b8e1aab603062e0
Rob Stryker <rob.stryker@jboss.com>
2012-02-09 14:20:09 +0000

    Ensuring credential page is only skipped if selection is an IUser or one is explicitly provided for import also
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38554 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/action/ImportApplicationAction.java
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/action/ImportApplicationAction.java
@@ -3,6 +3,7 @@ package org.jboss.tools.openshift.express.internal.ui.action;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.jface.viewers.ITreeSelection;
+import org.eclipse.jface.viewers.TreePath;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Display;
 import org.jboss.tools.openshift.express.internal.ui.OpenShiftUIActivator;
@@ -10,6 +11,7 @@ import org.jboss.tools.openshift.express.internal.ui.messages.OpenShiftExpressUI
 import org.jboss.tools.openshift.express.internal.ui.wizard.OpenShiftExpressApplicationWizard;
 
 import com.openshift.express.client.IApplication;
+import com.openshift.express.client.IUser;
 
 public class ImportApplicationAction extends AbstractAction {
 
@@ -25,6 +27,12 @@ public class ImportApplicationAction extends AbstractAction {
 			final IApplication application = (IApplication) treeSelection.getFirstElement();
 			//final IUser user = OpenShiftUIActivator.getDefault().getUser();
 			OpenShiftExpressApplicationWizard wizard = new OpenShiftExpressApplicationWizard();
+			TreePath[] paths = treeSelection.getPaths();
+			if( paths != null && paths.length == 1 ) {
+				Object user = paths[0].getParentPath().getLastSegment();
+				if( user instanceof IUser )
+					wizard.setInitialUser((IUser)user);
+			}
 			wizard.setSelectedApplication(application);
 			final IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(application.getName());
 			if(project.exists()) {
