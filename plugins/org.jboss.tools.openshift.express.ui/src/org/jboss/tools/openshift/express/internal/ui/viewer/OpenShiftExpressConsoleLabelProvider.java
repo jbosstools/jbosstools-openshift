ab5ba4935784eaddf1864197f7f370efbe787fdd
Andre Dietisheim <adietish@redhat.com>
2012-02-09 16:46:53 +0000

    added class header and author
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38570 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/viewer/OpenShiftExpressConsoleLabelProvider.java
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/viewer/OpenShiftExpressConsoleLabelProvider.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.openshift.express.internal.ui.viewer;
 
 import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider.IStyledLabelProvider;
@@ -14,6 +24,9 @@ import com.openshift.express.client.IEmbeddableCartridge;
 import com.openshift.express.client.IUser;
 import com.openshift.express.client.OpenShiftException;
 
+/**
+ * @author Xavier Coulon
+ */
 public class OpenShiftExpressConsoleLabelProvider implements IStyledLabelProvider, ILabelProvider {
 
 	@Override
