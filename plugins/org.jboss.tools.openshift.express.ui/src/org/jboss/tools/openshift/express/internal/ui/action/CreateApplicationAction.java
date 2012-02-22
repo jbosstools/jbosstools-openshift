db23ff9cb0789a09737fcf9a353f32e117f9fec6
Andre Dietisheim <adietish@redhat.com>
2012-02-22 09:11:40 +0000

    cleanup: added class header and author
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38964 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/action/CreateApplicationAction.java
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/action/CreateApplicationAction.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.openshift.express.internal.ui.action;
 
 import org.eclipse.jface.viewers.ITreeSelection;
@@ -10,6 +20,9 @@ import org.jboss.tools.openshift.express.internal.ui.wizard.OpenShiftExpressAppl
 
 import com.openshift.express.client.IUser;
 
+/**
+ * @author Xavier Coulon
+ */
 public class CreateApplicationAction extends AbstractAction  {
 
 	/**
