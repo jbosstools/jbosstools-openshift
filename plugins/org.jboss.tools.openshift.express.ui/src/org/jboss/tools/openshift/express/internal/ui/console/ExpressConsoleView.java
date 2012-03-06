7d828ba64eb16ed8dd35e71cac4b6eb4a882d2ed
Andre Dietisheim <adietish@redhat.com>
2012-03-06 08:39:50 +0000

    added class header and cleaned code
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@39298 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/console/ExpressConsoleView.java
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/console/ExpressConsoleView.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.openshift.express.internal.ui.console;
 
 import org.eclipse.swt.widgets.Composite;
@@ -6,6 +16,7 @@ import org.eclipse.ui.navigator.CommonNavigator;
 import org.eclipse.ui.navigator.CommonViewer;
 import org.jboss.tools.openshift.express.internal.core.console.IUserModelListener;
 import org.jboss.tools.openshift.express.internal.core.console.UserModel;
+import org.jboss.tools.openshift.express.internal.ui.utils.DisposeUtils;
 
 import com.openshift.express.client.IUser;
 
@@ -13,36 +24,37 @@ public class ExpressConsoleView extends CommonNavigator implements IUserModelLis
 	protected Object getInitialInput() {
 		return UserModel.getDefault();
 	}
+
 	protected CommonViewer createCommonViewer(Composite aParent) {
 		CommonViewer v = super.createCommonViewer(aParent);
 		UserModel.getDefault().addListener(this);
 		return v;
 	}
+
 	public void dispose() {
 		UserModel.getDefault().removeListener(this);
 		super.dispose();
 	}
-	
+
 	public void refreshViewer() {
-		Display.getDefault().asyncExec(new Runnable() { 
+		Display.getDefault().asyncExec(new Runnable() {
 			public void run() {
-				getCommonViewer().refresh();
+				if (!DisposeUtils.isDisposed(getCommonViewer())) {
+					getCommonViewer().refresh();
+				}
 			}
 		});
 	}
+
 	public void userAdded(IUser user) {
-		if( getCommonViewer() != null && !getCommonViewer().getTree().isDisposed()) {
-			refreshViewer();
-		}
+		refreshViewer();
 	}
+
 	public void userRemoved(IUser user) {
-		if( getCommonViewer() != null && !getCommonViewer().getTree().isDisposed()) {
-			refreshViewer();
-		}
+		refreshViewer();
 	}
+
 	public void userChanged(IUser user) {
-		if( getCommonViewer() != null && !getCommonViewer().getTree().isDisposed()) {
-			refreshViewer();
-		}
+		refreshViewer();
 	}
 }
