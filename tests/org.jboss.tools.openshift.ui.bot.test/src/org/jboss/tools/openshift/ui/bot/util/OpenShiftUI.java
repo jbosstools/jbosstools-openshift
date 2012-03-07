daf3c33d887a58606a7bea4b0dc7fa5bcb960cdd
Stefan Bunciak <sbunciak@redhat.comrawagner@redhat.com>
2012-03-07 13:54:58 +0000

    Bot tests for JBoss Tools 3.3.0.Beta1 OpenShift UI
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@39337 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.tools.openshift.ui.bot.test/src/org/jboss/tools/openshift/ui/bot/util/OpenShiftUI.java
@@ -0,0 +1,95 @@
+package org.jboss.tools.openshift.ui.bot.util;
+
+import java.util.List;
+import java.util.Vector;
+
+import org.jboss.tools.ui.bot.ext.gen.INewObject;
+import org.jboss.tools.ui.bot.ext.gen.IView;
+
+/**
+ * 
+ * Wrapper class for OpenShift UI tooling.
+ * 
+ * @author sbunciak
+ * 
+ */
+public class OpenShiftUI {
+
+	/**
+	 * 
+	 * Class representing OpenShift Console View
+	 *
+	 */
+	public static class Console {
+
+		public static final IView iView = new IView() {
+			@Override
+			public String getName() {
+				return "OpenShift Express Console";
+			}
+
+			@Override
+			public List<String> getGroupPath() {
+				List<String> l = new Vector<String>();
+				l.add("JBoss Tools");
+				return l;
+			}
+		};
+	}
+
+	/**
+	 * 
+	 * Class representing "navigation" to new OpenShift Express Application
+	 * 
+	 */
+	public static class NewApplication {
+		public static final INewObject iNewObject = new INewObject() {
+			@Override
+			public String getName() {
+				return "OpenShift Application";
+			}
+
+			@Override
+			public List<String> getGroupPath() {
+				List<String> l = new Vector<String>();
+				l.add("OpenShift");
+				return l;
+			}
+		};
+	}
+	
+	/**
+	 * List of available application type labels
+	 * 
+	 * @author sbunciak
+	 *
+	 */
+	public static class AppType {
+		
+		public static final String JBOSS = "jbossas-7";
+		public static final String JENKINS = "jenkins-1.4";
+		public static final String PERL = "perl-5.10";
+		public static final String PHP = "php-5.3";
+		public static final String PYTHON = "python-2.6";
+		public static final String RAW = "raw-0.1";
+		public static final String RUBY = "ruby-1.8";
+	}
+	
+	/**
+	 * List of available cartridge labels
+	 * 
+	 * @author sbunciak
+	 *
+	 */
+	public static class Cartridge {
+		
+		public static final String MONGODB = "mongodb-2.0";
+		public static final String JENKINS = "jenkins-1.4";
+		public static final String CRON = "cron-1.4";
+		public static final String MYSQL = "mysql-5.1";
+		public static final String POSTGRESQL = "postgresql-8.4";
+		public static final String PHPMYADMIN = "phpmyadmin-3.4";
+		public static final String METRICS = "metrics-0.1";
+		public static final String ROCKMONGO = "rockmongo-1.1";
+	}
+}
