8f6c4d0f6277146db60610331202d02ef99ce358
Xavier Coulon <xcoulon@gmail.com>
2012-05-14 16:53:56 +0000

    Fixed - JBIDE-11852 Improve responsiveness of User contextual menu in OpenShift Explorer https://issues.jboss.org/browse/JBIDE-11852
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@40991 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/action/DeleteDomainAction.java
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/action/DeleteDomainAction.java
@@ -16,14 +16,16 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.ITreeSelection;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
+import org.jboss.tools.common.ui.dialog.CheckboxMessageDialog;
 import org.jboss.tools.openshift.express.internal.core.console.UserDelegate;
+import org.jboss.tools.openshift.express.internal.ui.OpenShiftUIActivator;
 import org.jboss.tools.openshift.express.internal.ui.messages.OpenShiftExpressUIMessages;
 import org.jboss.tools.openshift.express.internal.ui.utils.Logger;
 
@@ -38,7 +40,7 @@ public class DeleteDomainAction extends AbstractAction {
 				.getImageDescriptor(ISharedImages.IMG_ETOOL_DELETE));
 	}
 
-	@Override
+	/*@Override
 	public void validate() {
 		boolean enable = false;
 		if (selection instanceof ITreeSelection
@@ -63,7 +65,7 @@ public class DeleteDomainAction extends AbstractAction {
 			}
 		}
 		setEnabled(enable);
-	}
+	}*/
 
 	@Override
 	public void run() {
@@ -75,28 +77,39 @@ public class DeleteDomainAction extends AbstractAction {
 				final IDomain domain = user.getDefaultDomain();
 				if (domain != null) {
 					boolean confirm = false;
-					confirm = MessageDialog.openConfirm(Display.getCurrent()
+					MessageDialog dialog = new CheckboxMessageDialog(Display.getCurrent()
+							.getActiveShell(), "Domain deletion", NLS.bind(
+									"You are about to delete the \"{0}\" domain.\n"
+											+ "Do you want to continue?",
+											domain.getId()), "Force applications deletion (data will be lost and operation cannot be undone)");
+					/*confirm = MessageDialog.openConfirm(Display.getCurrent()
 							.getActiveShell(), "Domain deletion", NLS.bind(
 							"You are about to delete the \"{0}\" domain.\n"
 									+ "Do you want to continue?",
-							domain.getId()));
+							domain.getId()));*/
+					int result = dialog.open();
+					if((result == CheckboxMessageDialog.INCLUDE_APPS) || (result == MessageDialog.OK)) {
+						confirm = true;
+					}
+					final boolean includeApps = ((result & CheckboxMessageDialog.INCLUDE_APPS) > 0);
 					if (confirm) {
 						Job job = new Job("Deleting OpenShift Domain...") {
 							@Override
 							protected IStatus run(IProgressMonitor monitor) {
 								try {
 									try {
-										domain.destroy();
+										domain.destroy(includeApps);
+										return Status.OK_STATUS;
 									} catch (OpenShiftException e) {
-										Logger.error(NLS.bind("Failed to delete domain \"{0}\"", domain.getId()), e);
+										return new Status(Status.ERROR, OpenShiftUIActivator.PLUGIN_ID, NLS.bind("Failed to delete domain \"{0}\"", domain.getId()), e);
+										
 									} catch (SocketTimeoutException e) {
-										Logger.error(NLS.bind("Failed to delete domain \"{0}\"", domain.getId()), e);
+										return new Status(Status.ERROR, OpenShiftUIActivator.PLUGIN_ID, NLS.bind("Failed to delete domain \"{0}\"", domain.getId()), e);
 									}
 								} finally {
 									monitor.done();
 								}
 
-								return Status.OK_STATUS;
 							}
 						};
 						job.setPriority(Job.SHORT);
