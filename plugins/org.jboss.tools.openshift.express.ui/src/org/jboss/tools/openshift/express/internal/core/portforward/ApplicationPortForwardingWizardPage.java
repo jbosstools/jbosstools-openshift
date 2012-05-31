ea1010728b2c0c9b7e9c1cd812d6f36e1559ee93
Andre Dietisheim <adietish@redhat.com>
2012-05-31 13:00:05 +0000

    added class header and author
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@41575 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/core/portforward/ApplicationPortForwardingWizardPage.java
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/core/portforward/ApplicationPortForwardingWizardPage.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.openshift.express.internal.core.portforward;
 
 import java.util.List;
@@ -38,6 +48,9 @@ import org.jboss.tools.openshift.express.internal.ui.wizard.AbstractOpenShiftWiz
 import com.openshift.client.IApplicationPortForwarding;
 import com.openshift.client.OpenShiftSSHOperationException;
 
+/**
+ * @author Xavier Coulon
+ */
 public class ApplicationPortForwardingWizardPage extends AbstractOpenShiftWizardPage {
 
 	private final ApplicationPortForwardingWizardModel wizardModel;
