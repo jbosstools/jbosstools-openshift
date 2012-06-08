4f73bbbf601fa47a16b0eca39584a66c4b214a7f
Andre Dietisheim <adietish@redhat.com>
2012-06-08 11:18:39 +0000

    [JBIDE-12134] moved validation from model to UI and implemented a MultiValidator in the way it is implemented in all other pages
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.3.x@41805 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/wizard/ProjectAndServerAdapterSettingsWizardPageModel.java
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/wizard/ProjectAndServerAdapterSettingsWizardPageModel.java
@@ -10,13 +10,10 @@
  ******************************************************************************/
 package org.jboss.tools.openshift.express.internal.ui.wizard;
 
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.osgi.util.NLS;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 import org.jboss.tools.common.ui.databinding.ObservableUIPojo;
-import org.jboss.tools.openshift.express.internal.ui.OpenShiftUIActivator;
 
 /**
  * @author Andre Dietisheim
@@ -24,6 +21,8 @@ import org.jboss.tools.openshift.express.internal.ui.OpenShiftUIActivator;
  * 
  */
 public class ProjectAndServerAdapterSettingsWizardPageModel extends ObservableUIPojo {
+
+	public static final String PROPERTY_APPLICATION_NAME = "applicationName";
 
 	/** whether this is a new project or not. */
 	public static final String PROPERTY_IS_NEW_PROJECT = "newProject";
@@ -34,16 +33,25 @@ public class ProjectAndServerAdapterSettingsWizardPageModel extends ObservableUI
 	/** whether this a server adapter should be created, or not. */
 	public static final String PROPERTY_CREATE_SERVER_ADAPTER = "createServerAdapter";
 
-	/** Whether the existing project is a valid one or not. */
-	public static final String PROPERTY_EXISTING_PROJECT_VALIDITY = "existingProjectValidity";
-
 	private IOpenShiftExpressWizardModel wizardModel;
-
-	private IStatus existingProjectValidity;
 
 	public ProjectAndServerAdapterSettingsWizardPageModel(IOpenShiftExpressWizardModel wizardModel) {
 		this.wizardModel = wizardModel;
 		setNewProject(true);
+		wizardModel.addPropertyChangeListener(IOpenShiftExpressWizardModel.APPLICATION_NAME, onWizardApplicationNameChanged());
+	}
+
+	/**
+	 * Listener to propagate the application name changes from the underlying WizardModel into this WizardPageModel, so that properties can be affected here, too.
+	 * @return
+	 */
+	private PropertyChangeListener onWizardApplicationNameChanged() {
+		return new PropertyChangeListener() {
+			@Override
+			public void propertyChange(PropertyChangeEvent evt) {
+				firePropertyChange(PROPERTY_APPLICATION_NAME, evt.getOldValue(), evt.getNewValue());
+			}
+		};
 	}
 
 	public void setNewProject(boolean newProject) {
@@ -51,7 +59,6 @@ public class ProjectAndServerAdapterSettingsWizardPageModel extends ObservableUI
 		if(wizardModel.isNewProject()) {
 			setProjectName(null);
 		}
-		validateExistingProject();
 	}
 
 	public boolean isNewProject() {
@@ -69,7 +76,6 @@ public class ProjectAndServerAdapterSettingsWizardPageModel extends ObservableUI
 
 	public void setProjectName(String projectName) {
 		firePropertyChange(PROPERTY_PROJECT_NAME, wizardModel.getProjectName(), wizardModel.setProjectName(projectName));
-		validateExistingProject();
 	}
 
 	public String getProjectName() {
@@ -79,46 +85,4 @@ public class ProjectAndServerAdapterSettingsWizardPageModel extends ObservableUI
 	public String getApplicationName() {
 		return wizardModel.getApplicationName();
 	}
-
-	public IStatus validateExistingProject() {
-		IStatus status = Status.OK_STATUS;
-		final String applicationName = getApplicationName();
-		if (isNewProject()) {
-			if (applicationName == null) {
-				status = OpenShiftUIActivator.createErrorStatus("You have to choose an application name");
-			} else {
-				final IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(applicationName);
-				if(project.exists()) {
-					status = OpenShiftUIActivator.createErrorStatus(
-							NLS.bind("A project named {0} already exists in the workspace.", applicationName));
-				}
-			}
-		} else {
-			final String projectName = wizardModel.getProjectName();
-			if (projectName == null || projectName.isEmpty()) {
-				status = OpenShiftUIActivator.createErrorStatus("Select an open project in the workspace.");
-			} else {
-				final IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
-				if (!project.exists()) {
-					status = OpenShiftUIActivator.createErrorStatus(
-							NLS.bind("The project {0} does not exist in your workspace.", projectName));
-				} else if (!project.isOpen()) {
-					status = OpenShiftUIActivator.createErrorStatus(
-							NLS.bind("The project {0} is not open.", projectName));
-				}
-			}
-		}
-		setExistingProjectValidity(status);
-		return status;
-	}
-
-	public void setExistingProjectValidity(IStatus status) {
-		firePropertyChange(PROPERTY_EXISTING_PROJECT_VALIDITY, this.existingProjectValidity,
-				this.existingProjectValidity = status);
-	}
-
-	public IStatus getExistingProjectValidity() {
-		return this.existingProjectValidity;
-	}
-
 }
