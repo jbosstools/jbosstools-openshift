dbb5d7847ca8610a4151b475c3aefaf8de2ff970
Rob Stryker <rob.stryker@jboss.com>
2012-09-17 05:10:25 +0000

    JBIDE-12613 cleanup on launch interfaces
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@43741 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/core/behaviour/ExpressLaunchDelegate.java
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/core/behaviour/ExpressLaunchDelegate.java
@@ -16,11 +16,12 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
 import org.eclipse.wst.server.core.IServer;
 import org.jboss.ide.eclipse.as.core.Trace;
+import org.jboss.ide.eclipse.as.core.server.IDelegatingServerBehavior;
 import org.jboss.ide.eclipse.as.core.server.IJBossLaunchDelegate;
 import org.jboss.ide.eclipse.as.core.server.internal.DelegatingServerBehavior;
-import org.jboss.ide.eclipse.as.core.server.internal.launch.DelegatingStartLaunchConfiguration;
 import org.jboss.ide.eclipse.as.core.util.JBossServerBehaviorUtils;
 import org.jboss.ide.eclipse.as.core.util.LaunchCommandPreferences;
 
@@ -31,7 +32,7 @@ public class ExpressLaunchDelegate implements IJBossLaunchDelegate {
 	}
 
 	@Override
-	public void actualLaunch(DelegatingStartLaunchConfiguration launchConfig,
+	public void actualLaunch(LaunchConfigurationDelegate launchConfig,
 			ILaunchConfiguration configuration, String mode, ILaunch launch,
 			IProgressMonitor monitor) throws CoreException {
 		// TODO Auto-generated method stub
@@ -41,7 +42,7 @@ public class ExpressLaunchDelegate implements IJBossLaunchDelegate {
 	@Override
 	public boolean preLaunchCheck(ILaunchConfiguration configuration,
 			String mode, IProgressMonitor monitor) throws CoreException {
-		DelegatingServerBehavior jbsBehavior = JBossServerBehaviorUtils.getServerBehavior(configuration);
+		IDelegatingServerBehavior jbsBehavior = JBossServerBehaviorUtils.getServerBehavior(configuration);
 		IStatus s = jbsBehavior.canStart(mode);
 
 		Trace.trace(Trace.STRING_FINEST, "Ensuring Server can start: " + s.getMessage()); //$NON-NLS-1$
@@ -49,8 +50,8 @@ public class ExpressLaunchDelegate implements IJBossLaunchDelegate {
 			throw new CoreException(jbsBehavior.canStart(mode));
 		if (LaunchCommandPreferences.isIgnoreLaunchCommand(jbsBehavior.getServer())) {
 			Trace.trace(Trace.STRING_FINEST, "Server is marked as ignore Launch. Marking as started."); //$NON-NLS-1$
-			jbsBehavior.setServerStarting();
-			jbsBehavior.setServerStarted();
+			((DelegatingServerBehavior)jbsBehavior).setServerStarting();
+			((DelegatingServerBehavior)jbsBehavior).setServerStarted();
 			return false;
 		}
 		return true;
