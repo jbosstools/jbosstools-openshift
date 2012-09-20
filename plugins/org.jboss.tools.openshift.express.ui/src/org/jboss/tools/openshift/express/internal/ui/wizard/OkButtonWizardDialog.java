0d66b7fb8853a9e9e4655d75c4675d6328a873ad
Andre Dietisheim <adietish@redhat.com>
2012-09-20 14:58:14 +0000

    [JBIDE-11912] corrected class header
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@43866 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/wizard/OkButtonWizardDialog.java
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/wizard/OkButtonWizardDialog.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.openshift.express.internal.ui.wizard;
 
 import org.eclipse.jface.dialogs.IDialogConstants;
@@ -7,6 +17,9 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
 
+/**
+ * @author Andre Dietisheim
+ */
 public class OkButtonWizardDialog extends WizardDialog {
 
 	public OkButtonWizardDialog(Shell parentShell, IWizard newWizard) {
