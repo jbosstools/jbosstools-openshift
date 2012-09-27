5a8a1216d6da7cfee14da24d08bfbd81af5eac2d
Andre Dietisheim <adietish@redhat.com>
2012-09-27 15:51:25 +0000

    split wizard package into distinct parts
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@44098 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/wizard/application/importoperation/project/AbstractProjectImportOperation.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.openshift.express.internal.ui.wizard.application.importoperation.project;
+
+import java.io.File;
+
+/**
+ * @author André Dietisheim <adietish@redhat.com>
+ */
+public class AbstractProjectImportOperation {
+
+	private File projectFolder;
+
+	public AbstractProjectImportOperation(File projectDirectory) {
+		this.projectFolder = projectDirectory;
+	}
+
+	protected File getProjectDirectory() {
+		return projectFolder;
+	}
+
+	protected boolean isReadable(File destination) {
+		return destination != null
+				&& destination.exists()
+				&& destination.canRead();
+	}
+}
---
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/wizard/application/importoperation/project/GeneralProjectImportOperation.java
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.openshift.express.internal.ui.wizard.application.importoperation.project;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * @author Andre Dietisheim <adietish@redhat.com>
+ * 
+ */
+public class GeneralProjectImportOperation extends AbstractProjectImportOperation {
+
+	public GeneralProjectImportOperation(File projectDirectory) {
+		super(projectDirectory);
+	}
+
+	public List<IProject> importToWorkspace(IProgressMonitor monitor)
+			throws CoreException, InterruptedException {
+
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IProject project = workspace.getRoot().getProject(getProjectDirectory().getName());
+		overwriteExistingProject(project, workspace, monitor);
+		importToWorkspace(getProjectDirectory(), workspace, monitor);
+		return Collections.singletonList(project);
+	}
+
+	private void importToWorkspace(File projectDirectory, IWorkspace workspace, IProgressMonitor monitor)
+			throws CoreException {
+		String projectName = projectDirectory.getName();
+		IProjectDescription description = workspace.newProjectDescription(projectName);
+		description.setLocation(Path.fromOSString(projectDirectory.getAbsolutePath()));
+		IProject project = workspace.getRoot().getProject(projectName);
+		project.create(description, monitor);
+		project.open(IResource.BACKGROUND_REFRESH, monitor);
+	}
+
+	private void overwriteExistingProject(final IProject project, IWorkspace workspace, IProgressMonitor monitor)
+			throws CoreException {
+		if (project == null
+				|| !project.exists()) {
+			return;
+		}
+
+		final boolean[] overwrite = new boolean[1];
+		Display.getDefault().syncExec(new Runnable() {
+
+			public void run() {
+				overwrite[0] = MessageDialog.openQuestion(
+						PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+						"Overwrite project?",
+						NLS.bind(
+								"A project \"{0}\" already exists in the workspace.\n"
+										+ "If you want to import the OpenShift \"{0}\", the project in your workspace will "
+										+ "get overwritten and may not be recovered.\n\n"
+										+ "Are you sure that you want to overwrite the project \"{0}\" in your workspace?",
+								project.getName()));
+			}
+
+		});
+		if (overwrite[0]) {
+			project.delete(true, true, monitor);
+		}
+	}
+}
