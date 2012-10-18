ed63ff64bd94abc6665bab5859e2bff66fc67ea6
Andre Dietisheim <adietish@redhat.com>
2012-10-18 17:33:26 +0000

    [JBIDE-12898] various fixes, switching from in-binding validation to multivalidator-validation (which does not prevent non-valid values to make it to model)
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@44585 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/wizard/connection/ConnectionWizardPageModel.java
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/wizard/connection/ConnectionWizardPageModel.java
@@ -20,6 +20,7 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.osgi.util.NLS;
 import org.jboss.tools.common.ui.databinding.ObservableUIPojo;
 import org.jboss.tools.openshift.express.internal.core.connection.Connection;
+import org.jboss.tools.openshift.express.internal.core.connection.ConnectionUtils;
 import org.jboss.tools.openshift.express.internal.core.connection.ConnectionsModel;
 import org.jboss.tools.openshift.express.internal.ui.OpenShiftUIActivator;
 import org.jboss.tools.openshift.express.internal.ui.utils.CollectionUtils;
@@ -27,6 +28,7 @@ import org.jboss.tools.openshift.express.internal.ui.viewer.NewConnectionMarker;
 import org.jboss.tools.openshift.express.internal.ui.wizard.IConnectionAwareModel;
 
 import com.openshift.client.IUser;
+import com.openshift.client.InvalidCredentialsOpenShiftException;
 import com.openshift.client.NotFoundOpenShiftException;
 import com.openshift.client.OpenShiftException;
 import com.openshift.client.OpenShiftTimeoutException;
@@ -61,7 +63,7 @@ public class ConnectionWizardPageModel extends ObservableUIPojo {
 		Connection wizardModelConnection = wizardModel.getConnection();
 		this.selectedConnection = getSelectedConnection(wizardModelConnection);
 		this.isCreateNewConnection = getIsCreateNewConnection(selectedConnection);
-		this.editedConnection = createConnection(wizardModelConnection);
+		this.editedConnection = createConnection(selectedConnection);
 		this.servers = getServers(editedConnection);
 	}
 
@@ -71,7 +73,12 @@ public class ConnectionWizardPageModel extends ObservableUIPojo {
 
 	private Connection getSelectedConnection(Connection wizardModelConnection) {
 		if (wizardModelConnection == null) {
-			return new NewConnectionMarker();
+			Connection recentConnection = ConnectionsModel.getDefault().getRecentConnection();
+			if (recentConnection != null) {
+				return recentConnection;
+			} else {
+				return new NewConnectionMarker();
+			}
 		} else {
 			return wizardModelConnection;
 		}
@@ -114,6 +121,7 @@ public class ConnectionWizardPageModel extends ObservableUIPojo {
 	private void setEditedConnection(Connection connection) {
 		Connection oldValue = editedConnection;
 		this.editedConnection = connection;
+		resetValid();
 		firePropertyChange(PROPERTY_SERVER, oldValue.getHost(), this.editedConnection.getHost());
 		firePropertyChange(PROPERTY_USERNAME, oldValue.getUsername(), this.editedConnection.getUsername());
 		firePropertyChange(PROPERTY_PASSWORD, oldValue.getPassword(), this.editedConnection.getPassword());
@@ -137,8 +145,9 @@ public class ConnectionWizardPageModel extends ObservableUIPojo {
 		if (this.isDefaultServer != isDefaultServer) {
 			firePropertyChange(PROPERTY_USE_DEFAULTSERVER,
 					this.isDefaultServer, this.isDefaultServer = isDefaultServer);
+			resetValid();
 			if (isDefaultServer) {
-				setServer(editedConnection.getHost());
+				setServer(ConnectionUtils.getDefaultHostUrl());
 			}
 			resetValid();
 		}
@@ -235,6 +244,9 @@ public class ConnectionWizardPageModel extends ObservableUIPojo {
 				} catch (OpenShiftTimeoutException e) {
 					status = OpenShiftUIActivator.createErrorStatus(NLS.bind(
 							"Could not reach server at {0}. Connection timeouted.", editedConnection.getHost()));
+				} catch (InvalidCredentialsOpenShiftException e) {
+					status = OpenShiftUIActivator.createErrorStatus(NLS.bind(
+							"The credentials for user {0} are not valid", editedConnection.getUsername()));
 				} catch (OpenShiftException e) {
 					status = OpenShiftUIActivator.createErrorStatus(NLS.bind(
 							"The credentials for user {0} are not valid", editedConnection.getUsername()));
