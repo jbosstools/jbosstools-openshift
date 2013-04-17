27f0078820665a10a183d747855fc982a5f3614a
André Dietisheim <adietish@redhat.com>
2013-04-17 10:45:30 +0200

    [JBIDE-13933] corrected all occurrences of "setup" to "set up"

--- plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/wizard/application/ProjectAndServerAdapterSettingsWizardPage.java
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/wizard/application/ProjectAndServerAdapterSettingsWizardPage.java
@@ -59,7 +59,7 @@ public class ProjectAndServerAdapterSettingsWizardPage extends AbstractOpenShift
 
 	public static final String PREF_CONTENTASSISTKEY = "prefContentAssistKey";
 	
-	private static final String PAGE_TITLE_FORMAT = "Setup Project for OpenShift Application \"{0}\""; 
+	private static final String PAGE_TITLE_FORMAT = "Set up Project for OpenShift Application \"{0}\""; 
 	
 	private ProjectAndServerAdapterSettingsWizardPageModel pageModel;
 	private Text existingProjectNameText = null;
