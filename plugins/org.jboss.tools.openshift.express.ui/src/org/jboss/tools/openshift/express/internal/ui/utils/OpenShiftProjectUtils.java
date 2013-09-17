9640654b6f610ef9854eeda7b97e3d2f78b529d2
Rob Stryker <rob@oxbeef.net>
2013-09-18 02:52:19 +0800

    openshift reorg:  missed one core util

--- plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/utils/OpenShiftProjectUtils.java
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/utils/OpenShiftProjectUtils.java
@@ -10,45 +10,9 @@
  ******************************************************************************/
 package org.jboss.tools.openshift.express.internal.ui.utils;
 
-import java.io.File;
 
-import org.eclipse.core.resources.IFolder;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Path;
-
-public class OpenShiftProjectUtils {
-
-	private static final String FOLDER_DOT_OPENSHIFT = ".openshift";
-	private static final String FOLDER_MARKERS = FOLDER_DOT_OPENSHIFT + File.separatorChar + "markers";
-
-	public static boolean isOpenShiftProject(IProject project) {
-		IFolder folder = getOpenShiftFolder(project);
-		return folder != null
-				&& folder.isAccessible();
-	}
-	
-	public static IFolder getOpenShiftFolder(IProject project) {
-		return project.getFolder(FOLDER_DOT_OPENSHIFT);
-
-	}
-
-	public static IFolder getMarkersFolder(IProject project) {
-		return project.getFolder(new Path(FOLDER_MARKERS));
-	}
-
-	public static IFolder ensureMarkersFolderExists(IProject project, IProgressMonitor monitor) throws CoreException {
-		ensureExists(getOpenShiftFolder(project), monitor);
-		return ensureExists(getMarkersFolder(project), monitor);
-	}
-
-	private static IFolder ensureExists(IFolder folder, IProgressMonitor monitor) throws CoreException {
-		if (!folder.isAccessible()) {
-			folder.create(false, true, monitor);
-		}
-		return folder;
-
-	}
-	
+/**
+ * @deprecated
+ */
+public class OpenShiftProjectUtils extends org.jboss.tools.openshift.express.internal.core.util.OpenShiftProjectUtils {
 }
