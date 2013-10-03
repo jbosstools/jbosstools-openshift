c36c153e4384f0c42b0d4faa06e8afd58ae38984
Martes G Wigglesworth <mwigglesworth@redhat.com>
2013-10-03 14:11:10 -0400

    Updates to support issue #12

---
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/wizard/application/variables/ApplicationEnvironmentalVariableEditWizardPageModel.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Red Hat, Inc. - initial API and implementation
+ *
+ ******************************************************************************/
+package org.jboss.tools.openshift.express.internal.ui.wizard.application.variables;
+
+import org.jboss.tools.common.ui.databinding.ObservableUIPojo;
+
+/**
+ * @author Martes G Wigglesworth
+ *
+ */
+public class ApplicationEnvironmentalVariableEditWizardPageModel extends ObservableUIPojo {
+
+	/**
+	 * Constructs a new instance of ApplicationEnvironmentalVariableConfigurationWizardPageModel
+	 */
+	public ApplicationEnvironmentalVariableEditWizardPageModel() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	// #TODO - Create data bindings for all fields necessary for this view.
+
+}
