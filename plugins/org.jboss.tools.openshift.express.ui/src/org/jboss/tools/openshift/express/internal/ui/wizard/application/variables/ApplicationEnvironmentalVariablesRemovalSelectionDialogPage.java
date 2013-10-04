3183963a8c02aa43a10d927e4b07421dc288c8fb
Martes G Wigglesworth <mwigglesworth@redhat.com>
2013-10-04 14:29:40 -0400

    Updates for issue #5

---
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/wizard/application/variables/ApplicationEnvironmentalVariablesRemovalSelectionDialogPage.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Red Hat, Inc. - initial API and implementation
+ *
+ ******************************************************************************/
+package org.jboss.tools.openshift.express.internal.ui.wizard.application.variables;
+
+/**
+ * @author Martes G Wigglesworth
+ *
+ */
+public class ApplicationEnvironmentalVariablesRemovalSelectionDialogPage {
+
+	/**
+	 * Constructs a new instance of ApplicationEnvironmentalVariablesRemovalSelectionDialogPage
+	 */
+	public ApplicationEnvironmentalVariablesRemovalSelectionDialogPage() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
