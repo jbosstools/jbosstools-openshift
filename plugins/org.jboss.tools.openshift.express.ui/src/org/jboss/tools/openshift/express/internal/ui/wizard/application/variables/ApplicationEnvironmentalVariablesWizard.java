f0127da1de6a4b335a5e57625df8cd4b640fea35
Martes G Wigglesworth <mwigglesworth@redhat.com>
2013-10-09 17:44:29 -0400

    Attached the Env Variables button to its Wizard view skeleton. Created a true Wizard which is used in the button connection. Supports issue #1

---
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/wizard/application/variables/ApplicationEnvironmentalVariablesWizard.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Red Hat, Inc. - initial API and implementation
+ *
+ ******************************************************************************/
+package org.jboss.tools.openshift.express.internal.ui.wizard.application.variables;
+
+import org.eclipse.jface.wizard.Wizard;
+
+/**
+ * @author Martes G Wigglesworth
+ * 
+ */
+public class ApplicationEnvironmentalVariablesWizard extends Wizard {
+
+	/**
+	 * Constructs a new instance of ApplicationEnvironmentalVariablesWizard
+	 */
+	public ApplicationEnvironmentalVariablesWizard() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.wizard.Wizard#performFinish()
+	 */
+	@Override
+	public boolean performFinish() {
+		return true;
+	}
+	
+}
