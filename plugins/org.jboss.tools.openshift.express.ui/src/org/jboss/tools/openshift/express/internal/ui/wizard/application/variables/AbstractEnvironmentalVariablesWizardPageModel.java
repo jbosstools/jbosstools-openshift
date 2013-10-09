a4123b3cd8cd5a3b0a1ac521c2fb805d7a8a651e
Martes G Wigglesworth <mwigglesworth@redhat.com>
2013-10-09 13:22:16 -0400

    created abstract model object for use when creating page view models within the variables wizard. Supports issue #2

---
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/wizard/application/variables/AbstractEnvironmentalVariablesWizardPageModel.java
@@ -0,0 +1,99 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Red Hat, Inc. - initial API and implementation
+ *
+ ******************************************************************************/
+package org.jboss.tools.openshift.express.internal.ui.wizard.application.variables;
+
+import java.util.HashMap;
+
+import org.jboss.tools.common.ui.databinding.ObservableUIPojo;
+import org.jboss.tools.openshift.express.internal.core.connection.Connection;
+
+/**
+ * @author Martes G Wigglesworth 
+ *
+ */
+public abstract class AbstractEnvironmentalVariablesWizardPageModel extends ObservableUIPojo {
+
+	/**
+	 * @return the userConnection
+	 */
+	public Connection getUserConnection() {
+		return userConnection;
+	}
+
+	/**
+	 * @param userConnection the userConnection to set
+	 */
+	public void setUserConnection(Connection userConnection) {
+		this.userConnection = userConnection;
+	}
+
+	/**
+	 * @return the variablesDB
+	 */
+	public HashMap<String, String> getVariablesDB() {
+		return variablesDB;
+	}
+
+	/**
+	 * @param variablesDB the variablesDB to set
+	 */
+	public void setVariablesDB(HashMap<String, String> variablesDB) {
+		this.variablesDB = variablesDB;
+	}
+
+	/**
+	 * @return the pageTitle
+	 */
+	public String getPageTitle() {
+		return pageTitle;
+	}
+
+	/**
+	 * Constructs a new instance of AbstractEnvironmentalVariablesWizardPageModel
+	 */
+	public AbstractEnvironmentalVariablesWizardPageModel() {
+		pageTitle = null;
+		userConnection = null;
+		variablesDB = null;
+	}
+	
+	/**
+	 * 
+	 * @param newKey
+	 */
+	protected String add(String newKey,String newValue)
+	{
+		return this.variablesDB.put(newKey,newValue);
+	};
+	
+	/**
+	 * 
+	 * @param target
+	 */
+	protected String delete(String key)
+	{
+		return this.variablesDB.remove(key);
+	}
+	
+	/**
+	 * 
+	 * @param target
+	 * @param value
+	 */
+	protected String updateKey(String target,String updateValue)
+	{
+		return this.variablesDB.put(target, updateValue);
+	}
+	
+	private String pageTitle;
+	private Connection userConnection;
+	private HashMap<String,String> variablesDB;
+}
