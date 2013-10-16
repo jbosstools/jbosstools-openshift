f63c40aa89b8c1bf26377b0f5aa3608ab5547ed1
André Dietisheim <adietish@redhat.com>
2013-10-16 11:35:58 +0200

    [JBIDE-15653] use workspace locking in adapter

--- plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/core/behaviour/OpenShiftServer.java
+++ plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/core/behaviour/OpenShiftServer.java
@@ -25,7 +25,6 @@ import org.jboss.ide.eclipse.as.core.server.internal.DeployableServer;
 import org.jboss.ide.eclipse.as.core.server.internal.IExtendedPropertiesProvider;
 import org.jboss.ide.eclipse.as.core.server.internal.JBossServer;
 import org.jboss.ide.eclipse.as.core.server.internal.extendedproperties.ServerExtendedProperties;
-import org.jboss.ide.eclipse.as.core.util.ServerUtil;
 import org.jboss.ide.eclipse.as.wtp.core.util.ServerModelUtilities;
 import org.jboss.tools.openshift.express.internal.core.connection.ConnectionUtils;
 import org.jboss.tools.openshift.express.internal.core.util.UrlUtils;
@@ -76,4 +75,11 @@ public class OpenShiftServer extends DeployableServer implements IURLProvider, I
 	public ServerExtendedProperties getExtendedProperties() {
 		return new OpenShiftServerExtendedProperties(getServer());
 	}
+
+	@Override
+	public boolean isUseProjectSpecificSchedulingRuleOnPublish() {
+		// ensure we're locking the whole workspace while publishing.
+		return false;
+	}
+
 }
