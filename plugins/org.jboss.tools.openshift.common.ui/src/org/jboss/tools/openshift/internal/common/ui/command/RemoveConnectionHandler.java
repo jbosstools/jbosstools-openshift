2582d87ea16a9c8442e50a2f4ae3ef1417ff39c5
Jeff Cantrill <jcantril@redhat.com>
2015-02-25 16:54:24 -0500

    [JBIDE-19373] Fix recursive loop when trying to remove a connection

--- plugins/org.jboss.tools.openshift.common.ui/src/org/jboss/tools/openshift/internal/common/ui/command/RemoveConnectionHandler.java
+++ plugins/org.jboss.tools.openshift.common.ui/src/org/jboss/tools/openshift/internal/common/ui/command/RemoveConnectionHandler.java
@@ -41,7 +41,7 @@ public class RemoveConnectionHandler extends AbstractHandler {
 
 									@Override
 									public String toString(IConnection connection) {
-										return toString(connection);
+										return connection.toString();
 									}
 								})
 						)
