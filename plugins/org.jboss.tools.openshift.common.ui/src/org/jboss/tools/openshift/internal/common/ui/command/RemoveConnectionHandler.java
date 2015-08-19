8ec8ad21ff25a8c01e02e8c4bf24b6e7f5d0b974
Rob Stryker <rob@oxbeef.net>
2015-08-19 14:52:30 -0400

    JBIDE-20324 - command handler should only be taking action if selection is adaptable
    JBIDE-20324 - command handler should only be taking action if selection is adaptable
    
    JBIDE-20324 - command handler should only be taking action if selection is adaptable

--- plugins/org.jboss.tools.openshift.common.ui/src/org/jboss/tools/openshift/internal/common/ui/command/RemoveConnectionHandler.java
+++ plugins/org.jboss.tools.openshift.common.ui/src/org/jboss/tools/openshift/internal/common/ui/command/RemoveConnectionHandler.java
@@ -31,6 +31,9 @@ public class RemoveConnectionHandler extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IConnection[] connections = UIUtils.getElements(HandlerUtil.getCurrentSelection(event), IConnection.class);
+		if( connections.length == 0 )
+			return null;
+
 		if (MessageDialog.openConfirm(HandlerUtil.getActiveShell(event)
 				, "Remove connection"
 				, NLS.bind("You are about to remove the connection(s):\n{0}\n\n"
@@ -41,14 +44,16 @@ public class RemoveConnectionHandler extends AbstractHandler {
 
 									@Override
 									public String toString(IConnection connection) {
-										return "\n" + connection.toString();
+										return connection == null ? "" : ("\n" + connection.toString());
 									}
 								})
 						)
 				)) {
 			for(IConnection connection : connections){
-				ConnectionsRegistrySingleton.getInstance().remove(connection);
+				if( connection != null )
+					ConnectionsRegistrySingleton.getInstance().remove(connection);
 			}
+
 		}
 		return null;
 	}
