f55214634f975a017935e5b8fa534003677ac7b4
Fred Bricon <fbricon@gmail.com>
2015-08-21 09:29:05 -0400

    [JBIDE-20487] Add 'Show Properties' menu to OpenShift Explorer
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

---
+++ plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/handler/ShowPropertiesHandler.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+* Copyright (c) 2015 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.openshift.internal.ui.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.jboss.tools.openshift.internal.ui.OpenShiftUIActivator;
+
+import com.openshift.restclient.model.IResource;
+
+/**
+ * Handler for displaying {@link IResource} properties.
+ * 
+ * @author Fred Bricon
+ */
+public class ShowPropertiesHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
+		Display.getDefault().asyncExec(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					PlatformUI.getWorkbench()
+					.getActiveWorkbenchWindow()
+					.getActivePage()
+					.showView("org.eclipse.ui.views.PropertySheet");
+				} catch (PartInitException e) {
+					OpenShiftUIActivator.getDefault().getLogger().logError("Failed to show the Properties view", e);
+				}
+			}
+		});
+		return null;
+	}
+
+
+}
