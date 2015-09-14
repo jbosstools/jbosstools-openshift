9265bcd30ae57dd6335ed11cc8e366e17b8aabcd
Jeff Cantrill <jcantril@redhat.com>
2015-09-14 16:36:29 -0400

    [JBIDE-20599] Allow the handler to set the connection instead of recent

--- plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/handler/DeployImageHandler.java
+++ plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/handler/DeployImageHandler.java
@@ -10,6 +10,7 @@
  ******************************************************************************/
 package org.jboss.tools.openshift.internal.ui.handler;
 
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
