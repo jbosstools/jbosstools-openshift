4d0f0057b298a0eec281a5cfb9e76dabc0a6204f
Rob Stryker <rob@oxbeef.net>
2015-12-02 20:46:33 -0500

    JBIDE-21175 - ensure more reliable locating of vagrant command
    Refactored and extracted to cleaner utility method
    
    Further cleanup as per fred

--- plugins/org.jboss.tools.openshift.cdk.server/src/org/jboss/tools/openshift/cdk/server/core/internal/CDKConstantUtility.java
+++ plugins/org.jboss.tools.openshift.cdk.server/src/org/jboss/tools/openshift/cdk/server/core/internal/CDKConstantUtility.java
@@ -10,29 +10,51 @@
  ******************************************************************************/ 
 package org.jboss.tools.openshift.cdk.server.core.internal;
 
-import org.eclipse.debug.core.ILaunchConfiguration;
+import java.io.File;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.wst.server.core.IServer;
 import org.jboss.tools.openshift.cdk.server.core.internal.adapter.controllers.IExternalLaunchConstants;
+import org.jboss.tools.openshift.internal.common.core.util.CommandLocationBinary;
 
-// TODO - allow customization of this location
 public class CDKConstantUtility {
 	private static final String VAGRANT_LOCATION_LINUX = "/usr/bin/vagrant";
-
+	
+	// Seems weird?  See https://github.com/mitchellh/vagrant/issues/1652
+	private static final String VAGRANT_LOCATION_WINDOWS = "C:\\HashiCorp\\Vagrant\\bin\\vagrant.exe";
+	
+	private static CommandLocationBinary binary;
+	
 	public static String getVagrantLocation() {
-		return VAGRANT_LOCATION_LINUX;
+		return findVagrantLocation();
 	}
 	
 	public static String getVagrantLocation(IServer server) {
-		try {
-			ILaunchConfiguration lc = server.getLaunchConfiguration(false, new NullProgressMonitor());
-			if( lc != null ) {
-				return lc.getAttribute(IExternalLaunchConstants.ATTR_LOCATION, getVagrantLocation());
+		if( server != null ) {
+			try {
+				ILaunchConfiguration lc = server.getLaunchConfiguration(false, new NullProgressMonitor());
+				if( lc != null ) {
+					String ret = lc.getAttribute(IExternalLaunchConstants.ATTR_LOCATION, (String)null);
+					if( ret != null && new File(ret).exists())
+							return ret;
+				}
+			} catch(CoreException ce) {
+				// ignore, this is non-critical
 			}
-		} catch(CoreException ce) {
-			// ignore, this is non-critical
 		}
-		return VAGRANT_LOCATION_LINUX;
+		return findVagrantLocation();
+	}
+	
+	private static String findVagrantLocation() {
+		if( binary == null ) {
+			binary = new CommandLocationBinary("vagrant");
+			binary.addPlatformLocation(Platform.OS_LINUX, VAGRANT_LOCATION_LINUX);
+			binary.addPlatformLocation(Platform.OS_WIN32, VAGRANT_LOCATION_WINDOWS);
+			binary.setDefaultPlatform(Platform.OS_LINUX);
+		}
+		return binary.findLocation();
 	}
 }
