426b5df7609f11cbe4248be36e65ee8ca328a0fa
Jeff Cantrill <jcantril@redhat.com>
2016-01-06 15:58:35 -0500

    fix build

--- plugins/org.jboss.tools.openshift.cdk.server/src/org/jboss/tools/openshift/cdk/server/core/internal/adapter/controllers/VagrantLaunchUtility.java
+++ plugins/org.jboss.tools.openshift.cdk.server/src/org/jboss/tools/openshift/cdk/server/core/internal/adapter/controllers/VagrantLaunchUtility.java
@@ -166,8 +166,12 @@ public class VagrantLaunchUtility {
 	private static String getTimeoutError(List<String> output, List<String> err) {
 		StringBuilder msg = new StringBuilder();
 		msg.append("Process output:\n");
-		output.forEach(line -> msg.append("   ").append(line));
-		err.forEach(line -> msg.append("   ").append(line));
+        for(String line : output){
+		  msg.append("   ").append(line);
+        }
+        for(String line : err){
+		  msg.append("   ").append(line);
+        }
 		return msg.toString();
 	}
 	
