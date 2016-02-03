e2f552c39ffe220a58b316517af8810e7dfff61e
Fred Bricon <fbricon@gmail.com>
2016-02-03 10:49:16 -0500

    [JBIDE-21528] add Pod Status column to properties view
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/property/tabbed/OpenShiftResourcePropertySection.java
+++ plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/property/tabbed/OpenShiftResourcePropertySection.java
@@ -37,7 +37,6 @@ import org.jboss.tools.openshift.common.core.utils.StringUtils;
 import org.jboss.tools.openshift.core.OpenShiftAPIAnnotations;
 import org.jboss.tools.openshift.internal.common.ui.utils.DisposeUtils;
 import org.jboss.tools.openshift.internal.common.ui.utils.TableViewerBuilder;
-import org.jboss.tools.openshift.internal.common.ui.utils.TableViewerBuilder.IColumnLabelProvider;
 import org.jboss.tools.openshift.internal.common.ui.utils.UIUtils;
 import org.jboss.tools.openshift.internal.ui.models.IResourceUIModel;
 
@@ -51,15 +50,15 @@ import com.openshift.restclient.model.IResource;
  */
 public class OpenShiftResourcePropertySection extends AbstractPropertySection implements OpenShiftAPIAnnotations {
 
-	private TableViewer table;
-	private PropertySheetPage details;
-	private TabbedPropertySheetPage page;
+	protected TableViewer table;
+	protected PropertySheetPage details;
+	protected TabbedPropertySheetPage page;
 	private ISelectionProvider selectionProvider;
 	private String menuContributionId;
-	
+
 	public OpenShiftResourcePropertySection() {
 	}
-	
+
 	public OpenShiftResourcePropertySection(String menuContributionId) {
 		this.menuContributionId = menuContributionId;
 	}
@@ -76,40 +75,39 @@ public class OpenShiftResourcePropertySection extends AbstractPropertySection im
 		if (page != null) {
 			page.getSite().setSelectionProvider(selectionProvider);
 		}
-		
 	}
-	
+
 	protected void createContents(Composite parent, TabbedPropertySheetPage aTabbedPropertySheetPage) {
 		this.page = aTabbedPropertySheetPage;
 		parent.setLayout(new GridLayout());
-		
+
 		SashForm container = new SashForm(parent, SWT.VERTICAL);
 		GridData d = new GridData(GridData.FILL_BOTH);
 		d.widthHint = 100; //A dirty trick that keeps table from growing unlimitedly within scrolled parent composite.
 		container.setLayoutData(d);
 		Composite tableContainer = new Composite(container, SWT.NONE);
-		
+
 		tableContainer.setLayout(new FillLayout());
 		this.table = createTable(tableContainer);
 
 		details = new PropertySheetPage();
 		details.createControl(container);
 		selectionProvider = new ISelectionProvider() {
-			
+
 			@Override
 			public void setSelection(ISelection selection) {
 			}
-			
+
 			@Override
 			public void removeSelectionChangedListener(ISelectionChangedListener listener) {
 				table.removeSelectionChangedListener(listener);
 			}
-			
+
 			@Override
 			public ISelection getSelection() {
 				return table.getSelection();
 			}
-			
+
 			@Override
 			public void addSelectionChangedListener(ISelectionChangedListener listener) {
 				table.addSelectionChangedListener(listener);
@@ -121,39 +119,14 @@ public class OpenShiftResourcePropertySection extends AbstractPropertySection im
 		Table table = new Table(tableContainer, SWT.BORDER | SWT.FULL_SELECTION | SWT.V_SCROLL | SWT.H_SCROLL);
 		table.setLinesVisible(true);
 		table.setHeaderVisible(true);
-		TableViewer viewer = new TableViewerBuilder(table, tableContainer)
-			.contentProvider(new ObservableListContentProvider())
-			.sorter(new ViewerSorter() {
-				
-				@Override
-				public int compare(Viewer viewer, Object e1, Object e2) {
-					IResource r1 = ((IResourceUIModel)e1).getResource();
-					IResource r2 = ((IResourceUIModel)e2).getResource();
-					return r1.getName().compareTo(r2.getName());
-				}
-				
-			})
-			.column(new IColumnLabelProvider<IResourceUIModel>() {
-				@Override
-				public String getValue(IResourceUIModel model) {
-					return model.getResource().getName();
-				}
-			}).name("Name").align(SWT.LEFT).weight(1).minWidth(10).buildColumn()
-			.column(new IColumnLabelProvider<IResourceUIModel>() {
-				@Override
-				public String getValue(IResourceUIModel model) {
-					return model.getResource().getCreationTimeStamp();
-				}
-			}).name("Created").align(SWT.LEFT).weight(1).minWidth(5).buildColumn()
-			.column(new IColumnLabelProvider<IResourceUIModel>() {
-				@Override
-				public String getValue(IResourceUIModel model) {
-					return StringUtils.serialize(model.getResource().getLabels());
-				}
-			}).name("Labels").align(SWT.LEFT).weight(1).minWidth(25).buildColumn()
-			.buildViewer();
+		TableViewerBuilder tableViewerBuilder = new TableViewerBuilder(table, tableContainer)
+			.contentProvider(new ObservableListContentProvider());
+
+		setSorter(tableViewerBuilder);
+		addColumns(tableViewerBuilder);
+
+		TableViewer viewer = tableViewerBuilder.buildViewer();
 		viewer.addSelectionChangedListener(new ISelectionChangedListener() {
-			
 			@Override
 			public void selectionChanged(SelectionChangedEvent event) {
 				details.selectionChanged(null, event.getSelection());
@@ -162,14 +135,52 @@ public class OpenShiftResourcePropertySection extends AbstractPropertySection im
 		addContextMenu(viewer);
 		return viewer;
 	}
-	
+
+	protected void setSorter(TableViewerBuilder tableViewerBuilder) {
+		tableViewerBuilder.sorter(new ViewerSorter() {
+			@Override
+			public int compare(Viewer viewer, Object e1, Object e2) {
+				IResource r1 = ((IResourceUIModel)e1).getResource();
+				IResource r2 = ((IResourceUIModel)e2).getResource();
+				return r1.getName().compareTo(r2.getName());
+			}
+		});
+	}
+
+	protected void addColumns(TableViewerBuilder tableViewerBuilder) {
+		addNameColumn(tableViewerBuilder);
+		addCreatedColumn(tableViewerBuilder);
+		addLabelsColumn(tableViewerBuilder);
+	}
+
+	protected void addNameColumn(TableViewerBuilder tableViewerBuilder) {
+		tableViewerBuilder
+		.column((IResourceUIModel model) -> {
+				return model.getResource().getName();
+		}).name("Name").align(SWT.LEFT).weight(1).minWidth(15).buildColumn();
+	}
+
+	protected void addCreatedColumn(TableViewerBuilder tableViewerBuilder) {
+		tableViewerBuilder
+		.column((IResourceUIModel model) -> {
+				return model.getResource().getCreationTimeStamp();
+		}).name("Created").align(SWT.LEFT).weight(1).minWidth(5).buildColumn();
+	}
+
+	protected void addLabelsColumn(TableViewerBuilder tableViewerBuilder) {
+		tableViewerBuilder
+		.column((IResourceUIModel model) -> {
+				return StringUtils.serialize(model.getResource().getLabels());
+		}).name("Labels").align(SWT.LEFT).weight(1).minWidth(25).buildColumn();
+	}
+
 	protected void addContextMenu(TableViewer viewer) {
 		if (menuContributionId != null) {
 			final IMenuManager contextMenu = UIUtils.createContextMenu(viewer.getTable());
 			UIUtils.registerContributionManager(menuContributionId, contextMenu, viewer.getTable());
 		}
 	}
-	
+
 	@Override
 	public void setInput(IWorkbenchPart part, ISelection selection) {
 		super.setInput(part, selection);
@@ -177,7 +188,7 @@ public class OpenShiftResourcePropertySection extends AbstractPropertySection im
 		if(model == null) return;
 		ITabDescriptor tab = page.getSelectedTab();
 		if(tab == null) return;
-		
+
 		String id = tab.getId();
 		String property = org.apache.commons.lang.StringUtils.right(id, id.length() - id.lastIndexOf(".") - 1);
 		table.setInput(BeanProperties.list(property).observe(model));
--- plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/property/tabbed/PodsPropertySection.java
+++ plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/property/tabbed/PodsPropertySection.java
@@ -11,6 +11,12 @@
 
 package org.jboss.tools.openshift.internal.ui.property.tabbed;
 
+import org.eclipse.swt.SWT;
+import org.jboss.tools.openshift.internal.common.ui.utils.TableViewerBuilder;
+import org.jboss.tools.openshift.internal.ui.models.IResourceUIModel;
+
+import com.openshift.restclient.model.IPod;
+
 /**
  * Tabbed property section for displaying Pods.
  *  
@@ -21,5 +27,14 @@ public class PodsPropertySection extends OpenShiftResourcePropertySection {
 
 	public PodsPropertySection() {
 		super("popup:org.jboss.tools.openshift.ui.properties.tab.PodsTab");
+	}
+
+	protected void addColumns(TableViewerBuilder tableViewerBuilder) {
+		addNameColumn(tableViewerBuilder);
+		tableViewerBuilder.column((IResourceUIModel model) -> {
+				IPod pod = (IPod) model.getResource();
+				return pod.getStatus();
+		}).name("Status").align(SWT.LEFT).weight(1).minWidth(10).buildColumn();
+		addCreatedColumn(tableViewerBuilder);
 	}
 }
