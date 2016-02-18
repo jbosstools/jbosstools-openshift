2dd870e4ed7381a83618d290a87b0326aabcd168
Andre Dietisheim <adietish@redhat.com>
2016-02-18 22:02:23 +0100

    [JBIDE-21704] re-create deployment mapper upon connection change

--- plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/server/WizardFragmentUtils.java
+++ plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/server/WizardFragmentUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Red Hat Inc..
+ * Copyright (c) 2015-2016 Red Hat Inc..
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,6 +10,9 @@
  *******************************************************************************/
 package org.jboss.tools.openshift.internal.ui.server;
 
+import org.eclipse.jface.wizard.IWizard;
+import org.eclipse.jface.wizard.IWizardPage;
+import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.wst.server.ui.wizard.IWizardHandle;
 
@@ -19,4 +22,20 @@ public class WizardFragmentUtils {
 		return (WizardPage) handle;
 	}
 
+	public static IWizard getWizard(IWizardHandle handle) {
+		IWizardPage page = getWizardPage(handle);
+		if (page == null) {
+			return null;
+		}
+		return page.getWizard();
+	}
+
+	public static WizardDialog getWizardDialog(IWizardHandle handle) {
+		IWizard wizard = getWizard(handle);
+		if (wizard == null) {
+			return null;
+		}
+		return (WizardDialog) wizard.getContainer();
+	}
+	
 }
