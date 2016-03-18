9462828fcf3f84fcc6c728c1ddaf6f1cd04146a1
Rob Stryker <rob@oxbeef.net>
2016-03-18 12:45:33 -0400

    JBIDE-21908 - legacy launch code was still in effect, didn't delegate to launch controller

--- plugins/org.jboss.tools.openshift.core/src/org/jboss/tools/openshift/core/server/OpenShiftStartLaunchConfiguration.java
+++ plugins/org.jboss.tools.openshift.core/src/org/jboss/tools/openshift/core/server/OpenShiftStartLaunchConfiguration.java
@@ -10,44 +10,13 @@
  *******************************************************************************/
 package org.jboss.tools.openshift.core.server;
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.debug.core.ILaunch;
-import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
-import org.eclipse.jdt.launching.AbstractJavaLaunchConfigurationDelegate;
-import org.eclipse.wst.server.core.IServer;
-import org.eclipse.wst.server.core.IServerAttributes;
-import org.eclipse.wst.server.core.ServerUtil;
-import org.jboss.ide.eclipse.as.wtp.core.server.behavior.IControllableServerBehavior;
-import org.jboss.tools.openshift.internal.core.OpenShiftCoreActivator;
+import org.jboss.ide.eclipse.as.wtp.core.server.launch.ControllableServerLaunchConfiguration;
 
 /**
  * @author Andre Dietisheim
  */
 public class OpenShiftStartLaunchConfiguration 
-	extends AbstractJavaLaunchConfigurationDelegate 
-	implements ILaunchConfigurationDelegate {
+	extends ControllableServerLaunchConfiguration {
 
-	@Override
-	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) 
-			throws CoreException {
-		final IServer s = ServerUtil.getServer(configuration);
-		if( s == null ) {
-			throw new CoreException(OpenShiftCoreActivator.statusFactory().errorStatus("Unable to locate server from launch configuration."));
-		}
-		
-		OpenShiftServerBehaviour beh = getBehavior(s);
-		beh.setServerStarted();
-	}
-
-	public static OpenShiftServerBehaviour getBehavior(IServerAttributes server) {
-		OpenShiftServerBehaviour behavior = (OpenShiftServerBehaviour) server.getAdapter(IControllableServerBehavior.class);
-		if( behavior == null ) {
-			behavior = (OpenShiftServerBehaviour) server.loadAdapter(OpenShiftServerBehaviour.class, new NullProgressMonitor());
-		}
-		return behavior;
-	}
 
 }
