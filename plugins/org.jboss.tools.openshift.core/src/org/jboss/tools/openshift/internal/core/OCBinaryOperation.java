dd168123502653be4bb4fd482c9289af5183bd31
Xavier Coulon <xcoulon@redhat.com>
2016-03-22 18:58:57 +0100

    JBIDE-21326 - Server Adapter: Publishing is not telling me what it's doing
    Writing the lines returned by the RSync operation in the client in the console
     using an IServerConsoleWriter

--- plugins/org.jboss.tools.openshift.core/src/org/jboss/tools/openshift/internal/core/OCBinaryOperation.java
+++ plugins/org.jboss.tools.openshift.core/src/org/jboss/tools/openshift/internal/core/OCBinaryOperation.java
@@ -10,19 +10,30 @@
  ******************************************************************************/ 
 package org.jboss.tools.openshift.internal.core;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.MultiStatus;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.wst.server.core.IServer;
 import org.jboss.tools.openshift.common.core.utils.StringUtils;
+import org.jboss.tools.openshift.core.server.OpenShiftServerUtils;
+import org.jboss.tools.openshift.core.server.RSync;
 import org.jboss.tools.openshift.internal.core.preferences.OCBinary;
 
 import com.openshift.restclient.OpenShiftContext;
 import com.openshift.restclient.capability.IBinaryCapability;
+import com.openshift.restclient.model.IService;
 
 /**
  * @author Andre Dietisheim
  */
 public abstract class OCBinaryOperation {
 
-	public void run(MultiStatus status) {
+	/**
+	 * Runs the operation
+	 * @param status the status to update during the operation
+	 */
+	public void run(final MultiStatus status) {
 		String oldLocation = OpenShiftContext.get().get(IBinaryCapability.OPENSHIFT_BINARY_LOCATION);
 		String location = OCBinary.getInstance().getLocation();
 		OpenShiftContext.get().put(IBinaryCapability.OPENSHIFT_BINARY_LOCATION, location);
@@ -36,4 +47,34 @@ public abstract class OCBinaryOperation {
 	}
 	
 	protected abstract void runOCBinary(MultiStatus multiStatus);
+	
+	/**
+	 * Creates an {@link RSync}
+	 * @param server the {@link IServer} on which the {@code rsync} operation will be performed
+	 * @return the {@link RSync} to be used to execute the command.
+	 * @throws CoreException
+	 */
+	public static RSync createRSync(final IServer server) throws CoreException {
+		final String location = OCBinary.getInstance().getLocation();
+		if( location == null ) {
+			throw new CoreException(OpenShiftCoreActivator.statusFactory().errorStatus(
+					"Binary for oc-tools could not be found. Please open the OpenShift 3 Preference Page and set the location of the oc binary."));
+		}
+		
+		final IService service = OpenShiftServerUtils.getService(server);
+		if (service == null) {
+			throw new CoreException(OpenShiftCoreActivator.statusFactory().errorStatus(
+					NLS.bind("Server {0} could not determine the service to publish to.", server.getName())));
+		}
+
+		final String podPath = OpenShiftServerUtils.getPodPath(server);
+		if (StringUtils.isEmpty(podPath)) {
+			throw new CoreException(OpenShiftCoreActivator.statusFactory().errorStatus(
+					NLS.bind("Server {0} could not determine the destination directory to publish to.", server.getName())));
+		}
+		
+		return new RSync(service, podPath, server);
+	}
+
+
 }
