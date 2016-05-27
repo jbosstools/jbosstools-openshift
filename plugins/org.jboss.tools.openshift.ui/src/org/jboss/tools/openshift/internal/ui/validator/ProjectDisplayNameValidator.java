12dcd6aaf6887012372bd43af6ebbe738ae10aa9
dbocharov <dbocharo@redhat.com>
2016-05-27 11:39:09 +0200

    jbide-21068: added copyright headers + small fixes

--- plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/validator/ProjectDisplayNameValidator.java
+++ plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/validator/ProjectDisplayNameValidator.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.openshift.internal.ui.validator;
 
 import org.apache.commons.lang.StringUtils;
@@ -12,8 +22,9 @@ public class ProjectDisplayNameValidator implements IValidator {
 
 	@Override
 	public IStatus validate(Object value) {
-		if(value != null && !(value instanceof String))
+		if(!(value instanceof String)) {
 			return ValidationStatus.cancel("Display name is not an instance of a string");
+		}
 		String param = (String) value;
 		if(StringUtils.isNotEmpty(param)) {
 			if(param.indexOf(SWT.TAB) >= 0 || param.indexOf(SWT.LF) >= 0) {
--- tests/org.jboss.tools.openshift.test/src/org/jboss/tools/openshift/test/ui/validator/ProjectDisplayNameValidatorTest.java
+++ tests/org.jboss.tools.openshift.test/src/org/jboss/tools/openshift/test/ui/validator/ProjectDisplayNameValidatorTest.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.openshift.test.ui.validator;
 
 import org.jboss.tools.openshift.internal.ui.validator.ProjectDisplayNameValidator;
@@ -17,7 +27,11 @@ public class ProjectDisplayNameValidatorTest extends AbstractValidatorTest {
 	@Test
 	public void emptyDisplayNameShouldBeValid() {
 		assertPass("");
-		assertPass(null);
+	}
+	
+	@Test
+	public void nullDisplayNameShouldBeCanceled() {
+		assertCancel(null);
 	}
 	
 	@Test
