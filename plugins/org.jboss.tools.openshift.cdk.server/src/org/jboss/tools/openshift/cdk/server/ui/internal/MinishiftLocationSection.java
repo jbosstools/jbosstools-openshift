d4f84f3236d16abf4fe31a4c167582c424b52d4e
Rob Stryker <rob@oxbeef.net>
2017-04-11 15:31:22 -0400

    JBIDE-24216 - validate minishift location
    Cleanup

--- plugins/org.jboss.tools.openshift.cdk.server/src/org/jboss/tools/openshift/cdk/server/ui/internal/MinishiftLocationSection.java
+++ plugins/org.jboss.tools.openshift.cdk.server/src/org/jboss/tools/openshift/cdk/server/ui/internal/MinishiftLocationSection.java
@@ -10,9 +10,18 @@
  ******************************************************************************/ 
 package org.jboss.tools.openshift.cdk.server.ui.internal;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Properties;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.dialogs.IMessageProvider;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -20,11 +29,15 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.wst.server.core.IServerWorkingCopy;
 import org.jboss.tools.openshift.cdk.server.core.internal.adapter.CDK3Server;
+import org.jboss.tools.openshift.cdk.server.core.internal.adapter.controllers.CDKLaunchUtility;
+import org.jboss.tools.openshift.cdk.server.core.internal.adapter.controllers.CommandTimeoutException;
 
 public class MinishiftLocationSection extends AbstractLocationSection {
 
@@ -34,6 +47,13 @@ public class MinishiftLocationSection extends AbstractLocationSection {
 	private static String LOC_ATTR = CDK3Server.MINISHIFT_FILE;
 	
 	private Combo hypervisorCombo;
+	
+
+	private Job longValidation;
+	private Properties minishiftVersionProps = null;
+	private static String ERROR_KEY = CDK3ServerWizardFragment.ERROR_KEY;
+	private static String VERSION_KEY = CDK3ServerWizardFragment.VERSION_KEY; 
+
 	
 	public MinishiftLocationSection() {
 		super(SECTION_TITLE, LABEL_STRING, COMMAND_NAME, LOC_ATTR);
@@ -86,4 +106,78 @@ public class MinishiftLocationSection extends AbstractLocationSection {
 		return chooseFile(selected, shell);
 	}
 	
+	@Override
+	protected void validate() {
+		kickValidationJob();
+	}
+	
+	protected void validate2() {
+		String s = getErrorString();
+		if( s == null ) {
+			txtDecorator.hide();
+		} else {
+			txtDecorator.setDescriptionText(s);
+			txtDecorator.show();
+		}
+	}
+	
+	private synchronized void kickValidationJob() {
+		if( longValidation != null ) {
+			longValidation.cancel(); 
+		} 
+		Text t = getLocationText();
+		if( t == null || t.isDisposed()) {
+			return;
+		}
+		File f = new File(t.getText());
+		if( !f.exists() || !f.canExecute()) {
+			validate2();
+			return;
+		}
+		String homeDir = t.getText();
+		
+		longValidation = new Job("Validate minishift location") {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				Properties ret = new Properties();
+				try {
+					String[] lines = CDKLaunchUtility.call(homeDir, new String[] {"version"}, 
+						new File(homeDir).getParentFile(),
+						new HashMap<String,String>(), 5000, false);
+					String imploded = String.join("\n", Arrays.asList(lines));
+					ret.load(new ByteArrayInputStream(imploded.getBytes()));
+				} catch(IOException | CommandTimeoutException e )  {
+					ret.put(ERROR_KEY, e.getMessage());
+				}
+				minishiftVersionProps = ret;
+				Display.getDefault().asyncExec(new Runnable() {
+					public void run() {
+						validate2();
+					}
+				});
+				return Status.OK_STATUS;
+			}
+		};
+		longValidation.setSystem(true);
+		longValidation.schedule(750);
+	}
+	
+	private String getErrorString() {
+		// Subclass override
+		Text t = getLocationText();
+		if( t != null && !t.isDisposed()) {
+			String v = t.getText();
+			File f = new File(v);
+			if( !f.exists()) {
+				return "File " + v + " does not exist.";
+			} else if( !f.canExecute()) {
+				return "File " + v + " is not executable.";
+			} else if( minishiftVersionProps == null ) {
+				return "Unknown error when checking minishift version: " + v;
+			} else if( minishiftVersionProps.getProperty(ERROR_KEY) != null ) {
+				return minishiftVersionProps.getProperty(ERROR_KEY);
+			}
+		}
+		return null;
+	}
 }
