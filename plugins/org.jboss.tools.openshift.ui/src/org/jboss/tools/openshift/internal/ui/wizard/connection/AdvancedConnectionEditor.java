9725f4b8541e2eaf6465aacde8bca3ef81af0d7a
Rob Stryker <rob@oxbeef.net>
2017-08-30 17:12:59 -0400

    [JBIDE-24802] switching oc location validation to databinding
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/wizard/connection/AdvancedConnectionEditor.java
+++ plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/wizard/connection/AdvancedConnectionEditor.java
@@ -11,30 +11,41 @@
 package org.jboss.tools.openshift.internal.ui.wizard.connection;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.lang.StringUtils;
+import org.eclipse.core.databinding.Binding;
 import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.ValidationStatusProvider;
 import org.eclipse.core.databinding.beans.BeanProperties;
+import org.eclipse.core.databinding.observable.IObservable;
+import org.eclipse.core.databinding.observable.Realm;
+import org.eclipse.core.databinding.observable.list.IObservableList;
+import org.eclipse.core.databinding.observable.list.WritableList;
 import org.eclipse.core.databinding.observable.value.IObservableValue;
 import org.eclipse.core.databinding.observable.value.IValueChangeListener;
 import org.eclipse.core.databinding.observable.value.ValueChangeEvent;
+import org.eclipse.core.databinding.observable.value.WritableValue;
+import org.eclipse.core.databinding.validation.MultiValidator;
+import org.eclipse.core.databinding.validation.ValidationStatus;
 import org.eclipse.core.runtime.Assert;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.IJobChangeEvent;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.runtime.jobs.JobChangeAdapter;
+import org.eclipse.jface.databinding.fieldassist.ControlDecorationSupport;
 import org.eclipse.jface.databinding.swt.WidgetProperties;
-import org.eclipse.jface.fieldassist.ControlDecoration;
-import org.eclipse.jface.fieldassist.FieldDecoration;
-import org.eclipse.jface.fieldassist.FieldDecorationRegistry;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Button;
@@ -54,10 +65,10 @@ import org.jboss.tools.openshift.core.preferences.OpenShiftCorePreferences;
 import org.jboss.tools.openshift.internal.common.ui.connection.ConnectionWizardPageModel;
 import org.jboss.tools.openshift.internal.common.ui.connection.ConnectionWizardPageModel.IConnectionAdvancedPropertiesProvider;
 import org.jboss.tools.openshift.internal.common.ui.connection.IAdvancedConnectionPropertiesEditor;
+import org.jboss.tools.openshift.internal.common.ui.databinding.RequiredControlDecorationUpdater;
 import org.jboss.tools.openshift.internal.common.ui.databinding.TrimTrailingSlashConverter;
 import org.jboss.tools.openshift.internal.common.ui.databinding.TrimmingStringConverter;
 import org.jboss.tools.openshift.internal.common.ui.detailviews.BaseDetailsView;
-import org.jboss.tools.openshift.internal.common.ui.job.UIUpdatingJob;
 import org.jboss.tools.openshift.internal.common.ui.utils.DataBindingUtils;
 import org.jboss.tools.openshift.internal.common.ui.utils.DialogAdvancedPart;
 import org.jboss.tools.openshift.internal.core.preferences.OCBinaryVersionValidator;
@@ -76,8 +87,9 @@ public class AdvancedConnectionEditor extends BaseDetailsView implements IAdvanc
 	private IObservableValue registryURLObservable;
 	private IObservableValue clusterNamespaceObservable;
 	private IConnectionAdvancedPropertiesProvider connectionAdvancedPropertiesProvider;
-	private ControlDecoration decoration;
 	Map<String, Object> extendedProperties = null;
+	private IObservableValue<IStatus> ocLocationValidity = new WritableValue(Status.OK_STATUS, IStatus.class);
+	private IObservableValue<IStatus> ocVersionValidity = new WritableValue(Status.OK_STATUS, IStatus.class);
 	
 	@SuppressWarnings("unchecked")
 	@Override
@@ -134,8 +146,9 @@ public class AdvancedConnectionEditor extends BaseDetailsView implements IAdvanc
 				GridDataFactory.fillDefaults()
 					.align(SWT.LEFT, SWT.CENTER)
 					.applyTo(lblRegistry);
+				IObservableValue<Boolean> overrideOCObservable = WidgetProperties.selection().observe(overrideOC);
 				ValueBindingBuilder
-					.bind(WidgetProperties.selection().observeDelayed(DELAY, overrideOC))
+					.bind(overrideOCObservable)
 					.to(BeanProperties.value(AdvancedConnectionEditorModel.PROP_OC_OVERRIDE).observe(model))
 					.in(dbc);
 
@@ -143,11 +156,49 @@ public class AdvancedConnectionEditor extends BaseDetailsView implements IAdvanc
 				GridDataFactory.fillDefaults()
 					.align(SWT.FILL, SWT.CENTER).grab(true, false)
 					.applyTo(ocText);
+				IObservableValue<String> ocLocationObservable = WidgetProperties.text(SWT.Modify).observe(ocText);
+				Binding ocLocationBinding = ValueBindingBuilder
+					.bind(ocLocationObservable)
+					.converting(new TrimmingStringConverter())
+					.to(BeanProperties.value(AdvancedConnectionEditorModel.PROP_OC_OVERRIDE_LOCATION).observe(model))
+					.in(dbc);
+				
+				
+				overrideOCObservable.addValueChangeListener(new IValueChangeListener<Boolean>() {
+					@Override
+					public void handleValueChange(ValueChangeEvent<? extends Boolean> event) {
+						if( !overrideOCObservable.getValue()) {
+							ocLocationObservable.setValue(OpenShiftCorePreferences.INSTANCE.getOCBinaryLocation());
+						}
+					}
+				});
+				
+				ocLocationBinding.getValidationStatus().addValueChangeListener(new IValueChangeListener<IStatus>() {
 
-				IObservableValue ocLocationObservable = WidgetProperties.text(SWT.Modify).observeDelayed(DELAY, ocText);
-				ValueBindingBuilder.bind(ocLocationObservable).converting(new TrimmingStringConverter()).to(
-						BeanProperties.value(AdvancedConnectionEditorModel.PROP_OC_OVERRIDE_LOCATION).observe(model))
-						.in(dbc);
+					@Override
+					public void handleValueChange(ValueChangeEvent<? extends IStatus> event) {
+						String location = model.getOcOverrideLocation();
+						IStatus ocLocationStatus = validateOCLocation(location);
+						ocLocationValidity.setValue(ocLocationStatus);
+						if (ocLocationStatus.isOK()) {
+							ocVersionValidity.setValue(ValidationStatus.cancel("Verifying oc version..."));
+							OCVersionValidationJob job = new OCVersionValidationJob(model.getOcOverrideLocation());
+							job.addJobChangeListener(new JobChangeAdapter() {
+
+								@Override
+								public void done(IJobChangeEvent event) {
+									ocVersionValidity.getRealm().exec(() ->							
+										ocVersionValidity.setValue(job.getOCVersionValidity()));
+								}});
+							job.schedule();
+						}
+					}
+				});
+				ValueBindingBuilder
+					.bind(overrideOCObservable)
+					.to(WidgetProperties.enabled().observe(ocText))
+					.notUpdatingParticipant()
+					.in(dbc);
 
 				Button ocBrowse = new Button(advancedComposite, SWT.PUSH);
 				ocBrowse.setText("Browse...");
@@ -162,38 +213,57 @@ public class AdvancedConnectionEditor extends BaseDetailsView implements IAdvanc
 					public void widgetSelected(SelectionEvent e) {
 						FileDialog fd = new FileDialog(ocBrowse.getShell());
 						fd.setText(ocText.getText());
+						IPath p = new Path(ocText.getText());
+						if( p.segmentCount() > 1 ) {
+							fd.setFilterPath(p.removeLastSegments(1).toOSString());
+						}
 						String result = fd.open();
 						if (result != null) {
 							ocLocationObservable.setValue(result);
-							validateOCLocation(ocText.getText(), overrideOC.getSelection());
 						}
 					}
 				});
 
-				overrideOC.addSelectionListener(new SelectionAdapter() {
+				ValueBindingBuilder
+					.bind(overrideOCObservable)
+					.to(WidgetProperties.enabled().observe(ocBrowse))
+					.notUpdatingParticipant()
+					.in(dbc);
+				
+				ValidationStatusProvider ocValidator = new MultiValidator() {
+
+					public IObservableList getTargets() {
+						WritableList targets = new WritableList();
+						targets.add(ocLocationObservable);
+						targets.add(ocLocationValidity);
+						targets.add(ocVersionValidity);
+						return targets;
+					}
+					
 					@Override
-					public void widgetSelected(SelectionEvent e) {
-						ocText.setEnabled(overrideOC.getSelection());
-						ocBrowse.setEnabled(overrideOC.getSelection());
-						validateOCLocation(ocText.getText(), overrideOC.getSelection());
+					protected IStatus validate() {
+						// access all observables that the framework should listen to
+						Boolean overrideOC = overrideOCObservable.getValue();
+						String ocLocation = ocLocationObservable.getValue();
+						IStatus ocLocationStatus = ocLocationValidity.getValue();
+						IStatus ocVersionStatus = ocVersionValidity.getValue();
+
+						IStatus status = Status.OK_STATUS;
+						if (Boolean.TRUE.equals(overrideOC)) {
+							if (!ocLocationStatus.isOK()) {
+								status = ocLocationStatus;
+							} else {
+								status = ocVersionStatus;
+							}
+						}
+						return status;
 					}
-				});
+				};
 
-				ocText.addModifyListener(new ModifyListener() {
-					public void modifyText(ModifyEvent e) {
-						validateOCLocation(ocText.getText(), overrideOC.getSelection());
-					}
-				});
-
-				decoration = new ControlDecoration(ocText, SWT.LEFT | SWT.TOP);
-				FieldDecoration fieldDecoration = FieldDecorationRegistry.getDefault()
-						.getFieldDecoration(FieldDecorationRegistry.DEC_ERROR);
-				decoration.setImage(fieldDecoration.getImage());
-
-				ocText.setEnabled(overrideOC.getSelection());
-				ocBrowse.setEnabled(overrideOC.getSelection());
-
-				validateOCLocation(ocText.getText(), overrideOC.getSelection());
+				dbc.addValidationStatusProvider(ocValidator);
+				ControlDecorationSupport
+					.create(ocValidator, SWT.LEFT | SWT.TOP, null, new RequiredControlDecorationUpdater());
+				
 			}
 
 			protected GridLayoutFactory adjustAdvancedCompositeLayout(GridLayoutFactory gridLayoutFactory) {
@@ -206,95 +276,57 @@ public class AdvancedConnectionEditor extends BaseDetailsView implements IAdvanc
 		return composite;
 	}
 
-	private void validateOCLocation(String location, boolean override) {
-		if (override) {
-			String shallowError = validateOCLocationFast(location);
-			if (shallowError != null) {
-				// show decorator, set message to error
-				decoration.setDescriptionText(shallowError);
-				decoration.show();
-			} else {
-				kickOCLocationValidation(location);
-			}
-		} else
-			decoration.hide();
-	}
-
-	private String validateOCLocationFast(String location) {
-		String error = null;
+	private IStatus validateOCLocation(String location) {
+		IStatus validity = ValidationStatus.ok();
 		if (StringUtils.isBlank(location)) {
-			error = "OC Location cannot be empty";
+			validity = ValidationStatus.error("Please provide a location for the OC binary.");
 		} else {
 			File file = new File(location);
 			// Error messages have to be set to field editor, not directly to
 			// the page.
 			if (!file.exists()) {
-				error = (NLS.bind("{0} was not found.", file));
+				validity = ValidationStatus.error((NLS.bind("{0} was not found.", file)));
 			} else if (file.isDirectory()) {
-				error = (NLS.bind("OC Location must be a file.", file));
+				validity = ValidationStatus.error((NLS.bind("OC Location must be a file.", file)));
 			} else if (!file.canExecute()) {
-				error = (NLS.bind("{0} does not have execute permissions.", file));
+				validity = ValidationStatus.error(NLS.bind("{0} does not have execute permissions.", file));
 			}
 		}
-		return error;
+		return validity;
 	}
 
-	private class OCVersionUIJob extends UIUpdatingJob {
+	private class OCVersionValidationJob extends Job {
 
 	    private Version version;
-	    private String error;
 	    private String location;
-	    public OCVersionUIJob(String location) {
-	    	super("Checking oc binary...");
-	    	this.location = location;
+	    private IStatus ocVersionValidity = ValidationStatus.cancel("OC version not verified yet.");
+
+	    public OCVersionValidationJob(String location) {
+			super("Checking oc binary...");
+	    		this.location = location;
 	    }
 	    
         @Override
         protected IStatus run(IProgressMonitor monitor) {
-        	error = validateOCLocationFast(location);
-    		if( error == null ) {
-    			version = new OCBinaryVersionValidator(location).getVersion(monitor);
-    		}
-            if (monitor.isCanceled()) {
-            	return Status.CANCEL_STATUS;
-            }
-            return Status.OK_STATUS;
-        }
-
-        @Override
-        protected IStatus updateUI(IProgressMonitor monitor) {
-        	if( error == null ) {
-				if (Version.emptyVersion.equals(version)) {
-					error = ("Could not determine your OpenShift client version");
-				} else if( !OCBinaryVersionValidator.isCompatibleForPublishing(version)) {
-					error = NLS.bind("OpenShift client version 1.1.1 or higher is required to avoid rsync issues.", version);
-				}
-        	}
-        	
-        	//  now we have an error string
-        	if( error == null ) {
-        		// hide decorator
-        		decoration.hide();
-        	} else {
-        		// show decorator, set message to error
-        		decoration.setDescriptionText(error);
-        		decoration.show();
-        	}
-        	
-        	return Status.OK_STATUS;
-        }
-	}
-	
-	private UIUpdatingJob versionVerificationJob;
-	private void kickOCLocationValidation(String location) {
-		if( versionVerificationJob != null ) {
-			versionVerificationJob.cancel();
+			this.version = new OCBinaryVersionValidator(location).getVersion(monitor);
+			if (Version.emptyVersion.equals(version)) {
+				this.ocVersionValidity = ValidationStatus.error("Could not determine your OpenShift client version");
+			} else if (!OCBinaryVersionValidator.isCompatibleForPublishing(version)) {
+				this.ocVersionValidity = ValidationStatus.error(NLS.bind(
+						"OpenShift client version 1.1.1 or higher is required to avoid rsync issues.", version));
+			} else {
+				this.ocVersionValidity = ValidationStatus.ok();
+			}
+			if (monitor.isCanceled()) {
+				this.ocVersionValidity = ValidationStatus.cancel("OC version verification was cancelled.");
+			}
+			return Status.OK_STATUS;
 		}
-		
-		versionVerificationJob = new OCVersionUIJob(location);
-        versionVerificationJob.schedule();
+        
+        public IStatus getOCVersionValidity() {
+        		return ocVersionValidity;
+        }
 	}
-	
 	
 	@SuppressWarnings("unchecked")
 	protected void onSelectedConnectionChanged(IObservableValue selectedConnection) {
