6462280b1addd45bb6e2dfe7a411299b0b95dcc0
Jeff MAURY <jmaury@redhat.com>
2017-09-13 08:34:35 +0200

    [JBIDE-24145] - Command to tune memory requirements for a deployment
    Moved NumericValidator
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- plugins/org.jboss.tools.openshift.common.ui/src/org/jboss/tools/openshift/internal/common/ui/databinding/NumericValidator.java
+++ plugins/org.jboss.tools.openshift.common.ui/src/org/jboss/tools/openshift/internal/common/ui/databinding/NumericValidator.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     Red Hat, Inc. - initial API and implementation
  ******************************************************************************/
-package org.jboss.tools.openshift.internal.ui.validator;
+package org.jboss.tools.openshift.internal.common.ui.databinding;
 
 import java.util.function.Function;
 
