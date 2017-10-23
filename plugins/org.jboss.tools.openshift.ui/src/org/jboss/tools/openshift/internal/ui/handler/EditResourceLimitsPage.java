d70bd0f192f05c7c61d66dc31c30e3298021332f
André Dietisheim <adietish@redhat.com>
2017-10-23 15:02:27 +0200

    [JBIDE-25091] corrected resource limit unit labels (#1598)
    * corrected duplicate MiB entry in units combo
    * added "..." to the end of the context menu entry since
    this is required if a context menu item is opening up a dialog
    * corrected button in Edit Resource limits wizard from "Finish"
    to "OK" since we have only 1 page
    * made sure the combos for memory and cpu limits are equal in
    width
    * moved rc/dc retrieval to job
    
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/handler/EditResourceLimitsPage.java
+++ plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/handler/EditResourceLimitsPage.java
@@ -36,84 +36,114 @@ import org.jboss.tools.openshift.internal.common.ui.wizard.AbstractOpenShiftWiza
 import org.jboss.tools.openshift.internal.ui.OpenShiftUIMessages;
 import org.jboss.tools.openshift.internal.ui.wizard.common.EditResourceLimitsPageModel;
 
+import com.openshift.restclient.model.IContainer;
+
 public class EditResourceLimitsPage extends AbstractOpenShiftWizardPage {
-    public static final String[] MEMORY_SUFFIXES = {"M", "G", "Mi", "Gi", ""};
-    public static final String[] MEMORY_SUFFIXES_LABELS = {"MB", "GB", "MiB", "GiB", "MiB"};
-    public static final String[] CPU_SUFFIXES = {"m", "k", "M", "G", ""};
-    public static final String[] CPU_SUFFIXES_LABELS = {"millicores", "kcores", "Mcores", "Gcores", "cores"};
-  
-    private EditResourceLimitsPageModel model;
+	public static final String[] MEMORY_SUFFIXES = 
+		{ "M", "G", "Mi", "Gi", "" };
+	public static final String[] MEMORY_SUFFIXES_LABELS = 
+		{ "MB (1000 KB)", "GB (1000 MB)", "MiB (1024 KB)", "GiB (1024 MB)", "bytes" };
+	public static final String[] CPU_SUFFIXES = 
+		{ "m", "k", "M", "G", "" };
+	public static final String[] CPU_SUFFIXES_LABELS = 
+		{ "millicores", "kcores", "Mcores", "Gcores", "cores" };
 
-    public EditResourceLimitsPage(EditResourceLimitsPageModel model, IWizard wizard) {
-        super(OpenShiftUIMessages.EditResourceLimitsPageTitle,
-              NLS.bind(OpenShiftUIMessages.EditResourceLimitsPageDescription, model.getUpdatedReplicationController().getName()),
-              "EditResourceLimitsPage",
-              wizard);
-        this.model = model;
-    }
+	private EditResourceLimitsPageModel model;
 
-    @Override
-    protected void doCreateControls(Composite parent, DataBindingContext dbc) {
-        GridLayoutFactory.fillDefaults().applyTo(parent);
-        final Composite dialogArea = new Composite(parent, SWT.NONE);
-        GridDataFactory.fillDefaults().align(SWT.FILL, SWT.FILL).grab(true, true).applyTo(dialogArea);
-        GridLayoutFactory.fillDefaults().numColumns(3).margins(10, 10).spacing(20, SWT.DEFAULT).applyTo(dialogArea);
+	public EditResourceLimitsPage(EditResourceLimitsPageModel model, IWizard wizard) {
+		super(OpenShiftUIMessages.EditResourceLimitsPageTitle,
+				NLS.bind(OpenShiftUIMessages.EditResourceLimitsPageDescription,
+						model.getUpdatedReplicationController().getName()),
+				"EditResourceLimitsPage", wizard);
+		this.model = model;
+	}
 
-        Group group = new Group(dialogArea, SWT.NONE);
-        group.setText(OpenShiftUIMessages.MemoryLabel);
-        GridDataFactory.fillDefaults().span(3, 1).grab(true, false).align(SWT.FILL, SWT.FILL).applyTo(group);
-        GridLayoutFactory.fillDefaults().numColumns(3).margins(10, 10).spacing(10, 4).applyTo(group);
-        createResourceWidgets(dbc, group, OpenShiftUIMessages.RequestLabel, EditResourceLimitsPageModel.REQUESTS_MEMORY, MEMORY_SUFFIXES, MEMORY_SUFFIXES_LABELS);
-        createResourceWidgets(dbc, group, OpenShiftUIMessages.LimitLabel, EditResourceLimitsPageModel.LIMITS_MEMORY, MEMORY_SUFFIXES, MEMORY_SUFFIXES_LABELS);
-        
-        group = new Group(dialogArea, SWT.NONE);
-        group.setText(OpenShiftUIMessages.CPULabel);
-        GridDataFactory.fillDefaults().span(3, 1).grab(true, false).align(SWT.FILL, SWT.FILL).applyTo(group);
-        GridLayoutFactory.fillDefaults().numColumns(3).margins(10, 10).spacing(10, 4).applyTo(group);
-        createResourceWidgets(dbc, group, OpenShiftUIMessages.RequestLabel, EditResourceLimitsPageModel.REQUESTS_CPU, CPU_SUFFIXES, CPU_SUFFIXES_LABELS);
-        createResourceWidgets(dbc, group, OpenShiftUIMessages.LimitLabel, EditResourceLimitsPageModel.LIMITS_CPU, CPU_SUFFIXES, CPU_SUFFIXES_LABELS);
-    }
-    
-    private void createResourceWidgets(DataBindingContext dbc, Group group, String label, String property, String[] suffixes, String[] labels) {
-        // label
-        Label labelComp = new Label(group, SWT.NONE);
-        labelComp.setText(label);
-        GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER).applyTo(labelComp);
+	@Override
+	protected void doCreateControls(Composite parent, DataBindingContext dbc) {
+		GridLayoutFactory.fillDefaults().applyTo(parent);
+		final Composite dialogArea = new Composite(parent, SWT.NONE);
+		GridDataFactory.fillDefaults()
+			.align(SWT.FILL, SWT.FILL).grab(true, true)
+			.applyTo(dialogArea);
+		GridLayoutFactory.fillDefaults()
+			.numColumns(3).margins(10, 10).spacing(20, SWT.DEFAULT)
+			.applyTo(dialogArea);
 
-        // scale spinner
-        Text text = new Text(group, SWT.BORDER);
-        GridDataFactory.fillDefaults().align(SWT.FILL, SWT.FILL).grab(true, false).applyTo(text);
-        ComboViewer combo = new ComboViewer(group);
-        combo.setContentProvider(ArrayContentProvider.getInstance());
-        combo.setInput(suffixes);
-        combo.setLabelProvider(new LabelProvider() {
+		Group group = new Group(dialogArea, SWT.NONE);
+		group.setText(OpenShiftUIMessages.MemoryLabel);
+		GridDataFactory.fillDefaults().span(3, 1).grab(true, false).align(SWT.FILL, SWT.FILL).applyTo(group);
+		GridLayoutFactory.fillDefaults().numColumns(3).margins(10, 10).spacing(10, 4).applyTo(group);
+		createResourceWidgets(OpenShiftUIMessages.RequestLabel, EditResourceLimitsPageModel.REQUESTS_MEMORY, 
+				MEMORY_SUFFIXES, MEMORY_SUFFIXES_LABELS, group, dbc);
+		createResourceWidgets(OpenShiftUIMessages.LimitLabel, EditResourceLimitsPageModel.LIMITS_MEMORY,
+				MEMORY_SUFFIXES, MEMORY_SUFFIXES_LABELS, group, dbc);
 
-            @Override
-            public String getText(Object element) {
-                for(int i=0; i < suffixes.length;++i) {
-                    if (element.equals(suffixes[i])) {
-                        element = labels[i];
-                        break;
-                    }
-                }
-                return (String) element;
-            }
-            
-        });
+		group = new Group(dialogArea, SWT.NONE);
+		group.setText(OpenShiftUIMessages.CPULabel);
+		GridDataFactory.fillDefaults().span(3, 1).grab(true, false).align(SWT.FILL, SWT.FILL).applyTo(group);
+		GridLayoutFactory.fillDefaults().numColumns(3).margins(10, 10).spacing(10, 4).applyTo(group);
+		createResourceWidgets(OpenShiftUIMessages.RequestLabel, EditResourceLimitsPageModel.REQUESTS_CPU,
+				CPU_SUFFIXES, CPU_SUFFIXES_LABELS, group, dbc);
+		createResourceWidgets(OpenShiftUIMessages.LimitLabel, EditResourceLimitsPageModel.LIMITS_CPU,
+				CPU_SUFFIXES, CPU_SUFFIXES_LABELS, group, dbc);
+	}
 
-        IObservableValue textObservable = WidgetProperties.text(SWT.Modify).observe(text);
-        IObservableValue comboObservable = ViewerProperties.singleSelection().observe(combo);
-        IObservableValue master = BeanProperties.value(EditResourceLimitsPageModel.SELECTED_CONTAINER).observe(model);
-        ValueBindingBuilder.bind(textObservable)
-                .validatingAfterGet(new NumericValidator("integer", Integer::parseInt))
-                .converting(new AggregatingConverter(comboObservable, true))
-                .to(PojoProperties.value(property).observeDetail(master))
-                .converting(new KeywordConverter(suffixes, true))
-                .in(dbc);
-        ValueBindingBuilder.bind(comboObservable)
-        .converting(new AggregatingConverter(textObservable, false))
-        .to(PojoProperties.value(property).observeDetail(master))
-        .converting(new KeywordConverter(suffixes, false))
-        .in(dbc);
-    }
+	private void createResourceWidgets(String label, String property, String[] suffixes, String[] labels, 
+			Group parent, DataBindingContext dbc) {
+		// label
+		Label labelComp = new Label(parent, SWT.NONE);
+		labelComp.setText(label);
+		GridDataFactory.fillDefaults()
+			.align(SWT.FILL, SWT.CENTER)
+			.applyTo(labelComp);
+
+		// value text
+		Text text = new Text(parent, SWT.BORDER);
+		GridDataFactory.fillDefaults()
+			.align(SWT.FILL, SWT.FILL).grab(true, false)
+			.applyTo(text);
+
+		// unit combo
+		ComboViewer combo = new ComboViewer(parent);
+		GridDataFactory.fillDefaults()
+			.align(SWT.FILL, SWT.CENTER).hint(140, SWT.DEFAULT)
+			.applyTo(combo.getControl());
+		combo.setContentProvider(ArrayContentProvider.getInstance());
+		combo.setInput(suffixes);
+		combo.setLabelProvider(new LabelProvider() {
+
+			@Override
+			public String getText(Object element) {
+				return getLabelForSuffix(element, suffixes, labels);
+			}
+
+			private String getLabelForSuffix(Object element, String[] suffixes, String[] labels) {
+				String label = (String) element;
+				for (int i = 0; i < suffixes.length; ++i) {
+					if (element.equals(suffixes[i])) {
+						label = labels[i];
+						break;
+					}
+				}
+				return label;
+			}
+		});
+
+		IObservableValue<String> valueObservable = WidgetProperties.text(SWT.Modify).observe(text);
+		IObservableValue<String> selectedUnitObservable = ViewerProperties.singleSelection().observe(combo);
+		IObservableValue<IContainer> master = BeanProperties.value(EditResourceLimitsPageModel.SELECTED_CONTAINER).observe(model);
+		ValueBindingBuilder
+			.bind(valueObservable)
+			.validatingAfterGet(new NumericValidator("integer", Integer::parseInt))
+			.converting(new AggregatingConverter(selectedUnitObservable, true))
+			.to(PojoProperties.value(property).observeDetail(master))
+			.converting(new KeywordConverter(suffixes, true))
+			.in(dbc);
+		ValueBindingBuilder
+			.bind(selectedUnitObservable)
+			.converting(new AggregatingConverter(valueObservable, false))
+			.to(PojoProperties.value(property).observeDetail(master))
+			.converting(new KeywordConverter(suffixes, false))
+			.in(dbc);
+	}
 }
--- plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/wizard/common/EditResourceLimitsHandler.java
+++ plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/wizard/common/EditResourceLimitsHandler.java
@@ -17,19 +17,20 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.osgi.util.NLS;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.jboss.tools.common.ui.WizardUtils;
+import org.eclipse.ui.progress.UIJob;
 import org.jboss.tools.openshift.core.connection.Connection;
 import org.jboss.tools.openshift.core.connection.ConnectionsRegistryUtil;
+import org.jboss.tools.openshift.internal.common.core.job.JobChainBuilder;
 import org.jboss.tools.openshift.internal.common.ui.utils.UIUtils;
+import org.jboss.tools.openshift.internal.common.ui.wizard.OkCancelButtonWizardDialog;
 import org.jboss.tools.openshift.internal.core.util.ResourceUtils;
 import org.jboss.tools.openshift.internal.ui.OpenShiftUIActivator;
 import org.jboss.tools.openshift.internal.ui.OpenShiftUIMessages;
 import org.jboss.tools.openshift.internal.ui.handler.EditResourceLimitsPage;
-import org.jboss.tools.openshift.internal.ui.utils.ResourceWrapperUtils;
 
 import com.openshift.restclient.ResourceKind;
 import com.openshift.restclient.model.IPod;
@@ -42,92 +43,125 @@ import com.openshift.restclient.model.IService;
  */
 public class EditResourceLimitsHandler extends AbstractHandler {
 
-    @Override
-    public Object execute(ExecutionEvent event) throws ExecutionException {
-        IReplicationController dcOrRc = getReplicationControllerOrDeploymentConfig(
-                getSelectedElement(event, IResource.class));
-        if (dcOrRc == null) {
-            IResource resource = ResourceWrapperUtils
-                    .getResource(UIUtils.getFirstElement(HandlerUtil.getCurrentSelection(event)));
-            return OpenShiftUIActivator.statusFactory()
-                    .errorStatus(NLS.bind(
-                            "Could not edit resources {0}: Could not find deployment config or replication controller",
-                            resource == null ? "" : resource.getName()));
-        }
-        editResources(event, dcOrRc, dcOrRc.getName());
-        return null;
-    }
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IResource resource = UIUtils.getFirstElement(UIUtils.getCurrentSelection(event), IResource.class);
+		RetrieveDCOrRCJob job = new RetrieveDCOrRCJob(resource);
+		new JobChainBuilder(job)
+		.runWhenSuccessfullyDone(new UIJob("Launching Edit Resource Limits Wizard...") {
 
-    protected <T> T getSelectedElement(ExecutionEvent event, Class<T> klass) {
-        ISelection selection = UIUtils.getCurrentSelection(event);
-        return UIUtils.getFirstElement(selection, klass);
-    }
+			@Override
+			public IStatus runInUIThread(IProgressMonitor monitor) {
+				IReplicationController dcOrRc = job.getDeplConfigOrReplController();
+				if (dcOrRc == null) {
+					return Status.CANCEL_STATUS;
+				}
+				editResources(HandlerUtil.getActiveShell(event), dcOrRc, dcOrRc.getName());
+				return Status.OK_STATUS;
+			}
+		})
+		.schedule();
+		return null;
+	}
 
-    private IReplicationController getReplicationControllerOrDeploymentConfig(IResource resource) {
-        if (resource == null) {
-            return null;
-        }
+	protected void editResources(Shell shell, IReplicationController rc, String name) {
+		EditResourceLimitsPageModel model = new EditResourceLimitsPageModel(rc);
+		EditResourceLimitsWizard wizard = new EditResourceLimitsWizard(model, "Edit resource limits");
+		new OkCancelButtonWizardDialog(shell, wizard).open();
+	}
 
-        Connection connection = ConnectionsRegistryUtil.getConnectionFor(resource);
-        IReplicationController rcOrDc = ResourceUtils.getDeploymentConfigFor(resource, connection);
-        if (null == rcOrDc) {
-        	if (resource instanceof IService) {
-        		rcOrDc = ResourceUtils.getReplicationControllerFor((IService) resource, resource.getProject().getResources(ResourceKind.REPLICATION_CONTROLLER));
-        	} else if (resource instanceof IReplicationController) {
-        		rcOrDc = (IReplicationController) resource;
-        	} else if (resource instanceof IPod) {
-        		rcOrDc = ResourceUtils.getDeploymentConfigOrReplicationControllerFor((IPod) resource);
-        		if (null == rcOrDc) {
-            		rcOrDc = ResourceUtils.getReplicationControllerFor((IPod) resource, resource.getProject().getResources(ResourceKind.REPLICATION_CONTROLLER));
-        		}
-        	}
-        }
-        return rcOrDc;
-    }
+	class EditResourceLimitsWizard extends Wizard {
+		private EditResourceLimitsPageModel model;
 
-    protected void editResources(ExecutionEvent event, IReplicationController rc, String name) {
-        EditResourceLimitsPageModel model = new EditResourceLimitsPageModel(rc);
-        EditResourceLimitsWizard wizard = new EditResourceLimitsWizard(model, "Edit resource limits");
-        WizardUtils.openWizardDialog(wizard, HandlerUtil.getActiveShell(event));
-     }
-    
-    class EditResourceLimitsWizard extends Wizard {
-        private EditResourceLimitsPageModel model;
+		public EditResourceLimitsWizard(EditResourceLimitsPageModel model, String title) {
+			this.model = model;
+			setWindowTitle(title);
+		}
 
-        public EditResourceLimitsWizard(EditResourceLimitsPageModel model, String title) {
-            this.model = model;
-            setWindowTitle(title);
-        }
+		@Override
+		public void addPages() {
+			addPage(new EditResourceLimitsPage(model, this));
+		}
 
-        @Override
-        public void addPages() {
-            addPage(new EditResourceLimitsPage(model, this));
-        }
+		@Override
+		public boolean performFinish() {
+			model.dispose();
+			new Job(NLS.bind(OpenShiftUIMessages.EditResourceLimitsJobTitle,
+					model.getUpdatedReplicationController().getName())) {
 
-        @Override
-        public boolean performFinish() {
-            model.dispose();
-            new Job(NLS.bind(OpenShiftUIMessages.EditResourceLimitsJobTitle, model.getUpdatedReplicationController().getName())) {
-                
-                @Override
-                protected IStatus run(IProgressMonitor monitor) {
-                    try {
-                        IReplicationController rc = model.getUpdatedReplicationController();
-                        Connection connection = ConnectionsRegistryUtil.getConnectionFor(rc);
-                        connection.updateResource(rc);
-                        return Status.OK_STATUS;
-                    } catch (Exception e) {
-                        String message = NLS.bind(OpenShiftUIMessages.EditResourceLimitsJobErrorMessage, model.getUpdatedReplicationController().getName());
-                        OpenShiftUIActivator.getDefault().getLogger().logError(message,e);
-                        return new Status(Status.ERROR, OpenShiftUIActivator.PLUGIN_ID, message, e);
-                    }
-                }
-            }.schedule();
-            return true;
-        }
-    }
+				@Override
+				protected IStatus run(IProgressMonitor monitor) {
+					try {
+						IReplicationController rc = model.getUpdatedReplicationController();
+						Connection connection = ConnectionsRegistryUtil.getConnectionFor(rc);
+						connection.updateResource(rc);
+						return Status.OK_STATUS;
+					} catch (Exception e) {
+						String message = NLS.bind(OpenShiftUIMessages.EditResourceLimitsJobErrorMessage,
+								model.getUpdatedReplicationController().getName());
+						OpenShiftUIActivator.getDefault().getLogger().logError(message, e);
+						return new Status(Status.ERROR, OpenShiftUIActivator.PLUGIN_ID, message, e);
+					}
+				}
+			}.schedule();
+			return true;
+		}
+	}
 
+	private class RetrieveDCOrRCJob extends Job {
 
+		private final IResource resource;
+		private IReplicationController deploymentConfOrReplController;
+
+		public RetrieveDCOrRCJob(IResource resource) {
+			super("Retrieve Deployment Config or Replication Controller...");
+			this.resource = resource;
+			this.deploymentConfOrReplController = null;
+		}
+
+		@Override
+		protected IStatus run(IProgressMonitor monitor) {
+			this.deploymentConfOrReplController = getDeploymentConfigOrReplicationController(resource);
+			if (deploymentConfOrReplController == null) {
+				return OpenShiftUIActivator.statusFactory().errorStatus(NLS.bind(
+						"Could not edit resources {0}: Could not find deployment config or replication controller",
+						resource == null ? "" : resource.getName()));
+			}
+			return Status.OK_STATUS;
+		}
+
+		/**
+		 * Gets a deployment config or a replication controller (if it can't find a
+		 * deployment config) for the given resource.
+		 * 
+		 * @param resource the resource to get a dc or rc for
+		 * @return
+		 */
+		private IReplicationController getDeploymentConfigOrReplicationController(final IResource resource) {
+			// TODO: move to ResourceUtils
+			if (resource == null) {
+				return null;
+			}
+
+			Connection connection = ConnectionsRegistryUtil.getConnectionFor(resource);
+			IReplicationController dcOrRc = ResourceUtils.getDeploymentConfigFor(resource, connection);
+			if (null == dcOrRc) {
+				if (resource instanceof IService) {
+					dcOrRc = ResourceUtils.getReplicationControllerFor((IService) resource,
+							resource.getProject().getResources(ResourceKind.REPLICATION_CONTROLLER));
+				} else if (resource instanceof IReplicationController) {
+					dcOrRc = (IReplicationController) resource;
+				} else if (resource instanceof IPod) {
+					dcOrRc = ResourceUtils.getDeploymentConfigOrReplicationControllerFor((IPod) resource);
+				}
+			}
+			return dcOrRc;
+		}
+
+		IReplicationController getDeplConfigOrReplController() {
+			return deploymentConfOrReplController;
+		}
+		
+	}
 
 }
-
