72f54945de344b719627edbe9cd4e332ecefd5ca
Dmitrii Bocharov <dbocharo@redhat.com>
2017-11-07 12:18:39 +0100

    jbide-24972: DeleteResourceHandlerTest is failing for Windows (#1605)
    Signed-off-by: Dmitrii Bocharov <dbocharo@redhat.com>

--- tests/org.jboss.tools.openshift.test/src/org/jboss/tools/openshift/test/internal/ui/handler/DeleteResourceHandlerTest.java
+++ tests/org.jboss.tools.openshift.test/src/org/jboss/tools/openshift/test/internal/ui/handler/DeleteResourceHandlerTest.java
@@ -1,20 +1,15 @@
 package org.jboss.tools.openshift.test.internal.ui.handler;
 
-import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.osgi.util.NLS;
 import org.jboss.tools.openshift.core.connection.Connection;
 import org.jboss.tools.openshift.core.connection.ConnectionsRegistryUtil;
 import org.jboss.tools.openshift.internal.common.ui.utils.UIUtils;
 import org.jboss.tools.openshift.internal.core.WatchManager;
-import org.jboss.tools.openshift.internal.ui.OpenShiftUIMessages;
 import org.jboss.tools.openshift.internal.ui.handler.DeleteResourceHandler;
 import org.jboss.tools.openshift.internal.ui.models.IProjectWrapper;
 import org.jboss.tools.openshift.internal.ui.models.IResourceWrapper;
@@ -22,16 +17,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.core.classloader.annotations.SuppressStaticInitializationFor;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import com.openshift.restclient.ResourceKind;
 import com.openshift.restclient.model.IProject;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ UIUtils.class, MessageDialog.class, WatchManager.class, ConnectionsRegistryUtil.class, NLS.class})
-@SuppressStaticInitializationFor({ "org.eclipse.swt.SWT", "org.eclipse.swt.widgets.Canvas",
-		"org.eclipse.swt.widgets.Shell", "org.eclipse.swt.widgets.Control" })
+@PrepareForTest({ WatchManager.class, ConnectionsRegistryUtil.class})
 public class DeleteResourceHandlerTest {
 
 	@Test
@@ -43,13 +35,6 @@ public class DeleteResourceHandlerTest {
 		IProjectWrapper projectWrapper = mock(IProjectWrapper.class);
 		when(projectWrapper.getWrapped()).thenReturn(project);
 		
-		IResourceWrapper<?, ?>[] resourceWrappers = new IResourceWrapper<?, ?>[] { projectWrapper };
-		PowerMockito.mockStatic(UIUtils.class);
-		PowerMockito.when(UIUtils.getElements(any(), any())).thenReturn(resourceWrappers);
-		
-		PowerMockito.mockStatic(MessageDialog.class);
-		PowerMockito.when(MessageDialog.openConfirm(any(), any(), any())).thenReturn(true);
-		
 		WatchManager watchManager = mock(WatchManager.class);
 		PowerMockito.mockStatic(WatchManager.class);
 		PowerMockito.when(WatchManager.getInstance()).thenReturn(watchManager);
@@ -57,15 +42,18 @@ public class DeleteResourceHandlerTest {
 		Connection connection = mock(Connection.class);
 		PowerMockito.mockStatic(ConnectionsRegistryUtil.class);
 		PowerMockito.when(ConnectionsRegistryUtil.getConnectionFor(eq(project))).thenReturn(connection);
-		
-		PowerMockito.mockStatic(NLS.class);
-		PowerMockito.doNothing().when(NLS.class, "initializeMessages", new Object[] {
-				"org.jboss.tools.openshift.internal.ui.OpenShiftUIMessages", OpenShiftUIMessages.class });
 
-		DeleteResourceHandler handler = new DeleteResourceHandler();
-		handler.execute(new ExecutionEvent());
+		DeleteResourceHandlerTestExtension handler = new DeleteResourceHandlerTestExtension();
+		handler.deleteResources(new IResourceWrapper<?, ?>[] { projectWrapper });
 		//TODO get rid of timeout here. Need to wait for callback from DeleteResourceJob for project
 		//difficult, because it's a job (runs async) and has overriden `doRun` method in OpenShiftJobs
 		verify(watchManager, timeout(200).times(1)).stopWatch(eq(project), eq(connection));
+	}
+	
+	class DeleteResourceHandlerTestExtension extends DeleteResourceHandler {
+	    
+	    public void deleteResources(final IResourceWrapper<?, ?>[] uiResources) {
+	        super.deleteResources(uiResources);
+	    }
 	}
 }
