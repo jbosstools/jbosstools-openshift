7c5c04b8603bef9d084bbac1012aca57afa733e5
Dmitrii Bocharov <dbocharo@redhat.com>
2018-03-26 18:41:58 +0200

    jbide-25581: remove unused import
    Signed-off-by: Dmitrii Bocharov <dbocharo@redhat.com>

--- plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/wizard/newapp/NewApplicationWizardModel.java
+++ plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/wizard/newapp/NewApplicationWizardModel.java
@@ -20,7 +20,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
