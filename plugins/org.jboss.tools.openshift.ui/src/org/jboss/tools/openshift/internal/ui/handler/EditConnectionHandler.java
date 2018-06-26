32909cef9accc64bb586454908caae0fa3f3c3f5
Dmitrii Bocharov <dbocharo@redhat.com>
2018-06-26 12:12:10 +0200

    jbide-25838: Explorer: expand connection to the level that it was expanded to after editing it
    Signed-off-by: Dmitrii Bocharov <dbocharo@redhat.com>

--- plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/handler/EditConnectionHandler.java
+++ plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/handler/EditConnectionHandler.java
@@ -10,16 +10,23 @@
  ******************************************************************************/
 package org.jboss.tools.openshift.internal.ui.handler;
 
+import java.util.Arrays;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.viewers.TreePath;
+import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.wizard.IWizard;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.jboss.tools.common.ui.WizardUtils;
 import org.jboss.tools.openshift.common.core.connection.IConnection;
 import org.jboss.tools.openshift.internal.common.ui.connection.ConnectionWizard;
+import org.jboss.tools.openshift.internal.common.ui.explorer.OpenShiftExplorerView;
 import org.jboss.tools.openshift.internal.common.ui.utils.UIUtils;
 import org.jboss.tools.openshift.internal.ui.models.ConnectionWrapper;
 
@@ -31,12 +38,28 @@ public class EditConnectionHandler extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 	    ConnectionWrapper connectionWrapper = UIUtils.getFirstElement(HandlerUtil.getCurrentSelection(event), ConnectionWrapper.class);
+	    int expandToLevel = findExpansionLevel(connectionWrapper);
         IConnection connection = connectionWrapper.getWrapped();
 	    IStatus status = openConnectionWizard(connection, event);
         if (Status.OK_STATUS.equals(status)) {
 		    connectionWrapper.refresh();
+		    getTreeViewer().expandToLevel(connectionWrapper, expandToLevel);
 		}
 		return null;
+	}
+
+	private int findExpansionLevel(ConnectionWrapper connectionWrapper) {
+        TreePath[] expandedTreePaths = getTreeViewer().getExpandedTreePaths();
+        return Arrays.stream(expandedTreePaths)
+                .filter(tp -> connectionWrapper.equals(tp.getFirstSegment()))
+                .map(tp -> tp.getSegmentCount())
+                .reduce(Integer.MIN_VALUE, Integer::max);
+	}
+	
+	private TreeViewer getTreeViewer() {
+	    IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+        OpenShiftExplorerView explorerView = (OpenShiftExplorerView)page.findView("org.jboss.tools.openshift.express.ui.explorer.expressConsoleView");
+        return explorerView.getCommonViewer();
 	}
 
 	protected IStatus openConnectionWizard(IConnection connection, ExecutionEvent event) {
