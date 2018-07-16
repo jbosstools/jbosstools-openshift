a913990318f500afb48e39125b3f2c133035b02f
Andre Dietisheim <adietish@redhat.com>
2018-07-16 17:17:55 +0200

    [JBIDE-25700] reorganized PodLogsHandler
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/handler/AbstractOpenShiftCliHandler.java
+++ plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/handler/AbstractOpenShiftCliHandler.java
@@ -10,6 +10,8 @@
  ******************************************************************************/
 package org.jboss.tools.openshift.internal.ui.handler;
 
+import static org.jboss.tools.openshift.core.preferences.IOpenShiftCoreConstants.OPEN_SHIFT_PREFERENCE_PAGE_ID;
+
 import org.apache.commons.lang.StringUtils;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -26,12 +28,11 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Link;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.dialogs.PreferencesUtil;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.jboss.tools.openshift.common.core.connection.IConnection;
 import org.jboss.tools.openshift.internal.core.preferences.OCBinary;
-
-import static org.jboss.tools.openshift.core.preferences.IOpenShiftCoreConstants.OPEN_SHIFT_PREFERENCE_PAGE_ID;
 
 /**
  * 
@@ -48,28 +49,30 @@ public abstract class AbstractOpenShiftCliHandler extends AbstractHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		String location = OCBinary.getInstance().getPath(getConnection(event));
 		if (StringUtils.isBlank(location)) {
-
-			final MessageDialog dialog = new MessageDialog(HandlerUtil.getActiveShell(event),
-					"Unknown executable location", null,
-					"The OpenShift Client '" + OCBinary.getInstance().getName() + "' executable can not be found.",
-					MessageDialog.ERROR, new String[] { IDialogConstants.OK_LABEL }, 0) {
-				@Override
-				protected Control createCustomArea(Composite parent) {
-					Composite container = new Composite(parent, SWT.NONE);
-					GridDataFactory.fillDefaults().align(SWT.FILL, SWT.FILL).grab(true, true).applyTo(container);
-					GridLayoutFactory.fillDefaults().applyTo(container);
-					Link link = new Link(container, SWT.WRAP);
-					link.setText("You must set the executable location in the <a>OpenShift 3 preferences</a>.");
-					link.addSelectionListener(new OpenPreferencesListener(this));
-					container.setFocus();
-					return container;
-				}
-			};
-			dialog.open();
+			showOCBinaryErrorMessage(HandlerUtil.getActiveShell(event));
 			return null;
 		}
 		handleEvent(event);
 		return null;
+	}
+
+	private void showOCBinaryErrorMessage(Shell shell) {
+		new MessageDialog(shell,
+				"Unknown executable location", null,
+				"The OpenShift Client '" + OCBinary.getInstance().getName() + "' executable can not be found.",
+				MessageDialog.ERROR, new String[] { IDialogConstants.OK_LABEL }, 0) {
+			@Override
+			protected Control createCustomArea(Composite parent) {
+				Composite container = new Composite(parent, SWT.NONE);
+				GridDataFactory.fillDefaults().align(SWT.FILL, SWT.FILL).grab(true, true).applyTo(container);
+				GridLayoutFactory.fillDefaults().applyTo(container);
+				Link link = new Link(container, SWT.WRAP);
+				link.setText("You must set the executable location in the <a>OpenShift 3 preferences</a>.");
+				link.addSelectionListener(new OpenPreferencesListener(this));
+				container.setFocus();
+				return container;
+			}
+		}.open();
 	}
 
 	private static class OpenPreferencesListener extends SelectionAdapter {
@@ -84,15 +87,12 @@ public abstract class AbstractOpenShiftCliHandler extends AbstractHandler {
 		public void widgetSelected(SelectionEvent e) {
 			dialog.close();
 			//Opening in asyncExec to workaround https://bugs.eclipse.org/471717 on OSX
-			Display.getDefault().asyncExec(new Runnable() {
-				@Override
-				public void run() {
+			Display.getDefault().asyncExec(() ->
 					PreferencesUtil
 							.createPreferenceDialogOn(Display.getDefault().getActiveShell(),
 									OPEN_SHIFT_PREFERENCE_PAGE_ID, new String[] { OPEN_SHIFT_PREFERENCE_PAGE_ID }, null)
-							.open();
-				}
-			});
+							.open()
+			);
 		}
 
 		@Override
--- plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/job/PodLogsJob.java
+++ plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/job/PodLogsJob.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Red Hat, Inc.
+ * Copyright (c) 2015-2018 Red Hat, Inc.
  * Distributed under license by Red Hat, Inc. All rights reserved.
  * This program is made available under the terms of the
  * Eclipse Public License v1.0 which accompanies this distribution,
@@ -50,7 +50,7 @@ public class PodLogsJob extends AbstractDelegatingMonitorJob {
 	private final Key key;
 
 	public PodLogsJob(IPod pod, String containerName) {
-		super("FollowPodLogsJob");
+		super("Displaying pod logs...");
 		this.key = new Key(pod, containerName);
 	}
 
