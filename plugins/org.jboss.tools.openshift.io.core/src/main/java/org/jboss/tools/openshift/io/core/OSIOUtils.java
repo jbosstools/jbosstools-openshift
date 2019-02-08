62273525a59939971f7d4e230611da1c3a8f7e27
Josef Kopriva <jkopriva@redhat.com>
2019-02-08 13:21:18 +0100

    JBIDE-26540 Update jjwt libraries to 0.10.5
    Signed-off-by: Josef Kopriva <jkopriva@redhat.com>

--- plugins/org.jboss.tools.openshift.io.core/src/main/java/org/jboss/tools/openshift/io/core/OSIOUtils.java
+++ plugins/org.jboss.tools.openshift.io.core/src/main/java/org/jboss/tools/openshift/io/core/OSIOUtils.java
@@ -11,11 +11,10 @@
 package org.jboss.tools.openshift.io.core;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.Date;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.jsonwebtoken.Claims;
