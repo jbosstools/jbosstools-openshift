ec97a4f4ec1cd91ea5b1e3fadc5bb4f25ed1405f
Andre Dietisheim <adietish@redhat.com>
2019-10-28 13:50:23 +0100

    [JBIDE-26869,JBIDE-26915] retry connect to crc & client 8.1.1-SNAPSHOT
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- plugins/org.jboss.tools.openshift.cdk.server/src/org/jboss/tools/openshift/internal/crc/server/core/listeners/ConfigureCRCFrameworksListener.java
+++ plugins/org.jboss.tools.openshift.cdk.server/src/org/jboss/tools/openshift/internal/crc/server/core/listeners/ConfigureCRCFrameworksListener.java
@@ -22,7 +22,9 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.osgi.util.NLS;
+import org.eclipse.ui.progress.UIJob;
 import org.eclipse.wst.server.core.IServer;
 import org.eclipse.wst.server.core.ServerEvent;
 import org.jboss.ide.eclipse.as.core.server.UnitedServerListener;
@@ -33,10 +35,13 @@ import org.jboss.tools.openshift.internal.cdk.server.core.listeners.CDKOpenshift
 import org.jboss.tools.openshift.internal.core.ocbinary.OCBinary;
 import org.jboss.tools.openshift.internal.crc.server.core.adapter.CRC100Server;
 
+import com.openshift.restclient.OpenShiftException;
 import com.openshift.restclient.authorization.IAuthorizationContext;
 
 public class ConfigureCRCFrameworksListener extends UnitedServerListener {
 
+	private static final int CONNECT_TRIES = 3;
+	private static final long RECONNECT_DELAY = 3 * 1000l;
 	private static final String CRC_HOST_URL = "https://api.crc.testing";
 	private static final int CRC_HOST_PORT = 6443;
 	private static final String CRC_DEV_USERNAME = "developer";
@@ -100,7 +105,43 @@ public class ConfigureCRCFrameworksListener extends UnitedServerListener {
 				ocLocation,
 				ConnectionsRegistrySingleton.getInstance());
 		connection.enablePromptCredentials(false);
-		connection.connect();
+		connect(connection);
+	}
+
+	private void connect(IConnection connection) {
+		boolean connected = false;
+		int runs = 0;
+		try {
+			while (!connected 
+					&& runs++ <= CONNECT_TRIES) {
+				try {
+					connected = connection.connect();
+				} catch (OpenShiftException ex) {
+					CDKCoreActivator.pluginLog().logError(ex);
+					Thread.sleep(RECONNECT_DELAY);
+				}
+			}
+			warnUnconnected(connected);
+		} catch (InterruptedException ie) {
+			CDKCoreActivator.pluginLog().logError(ie);
+		}
+	}
+
+	private void warnUnconnected(boolean connected) {
+		if (!connected) {
+			new UIJob("Warning: Wait and Refresh connection") {
+
+				@Override
+				public IStatus runInUIThread(IProgressMonitor monitor) {
+					MessageDialog.openWarning(getDisplay().getActiveShell(), 
+							"Wait and Refresh connection",
+							"Could not connect to the running CRC instance."
+							+ "\nIt may not fully up and running yet. Wait and try refreshing the connection.");
+					return Status.OK_STATUS;
+				}
+			}
+			.schedule();
+		}
 	}
 
 	private String getOcLocation(IServer server) {
@@ -117,7 +158,7 @@ public class ConfigureCRCFrameworksListener extends UnitedServerListener {
 	private String getAdminPassword(IServer server) {
 		File passwordFile = findAdminPasswordFile(server);
 		String passwordContent = null;
-		if( passwordFile != null ) {
+		if (passwordFile != null) {
 			try {
 				passwordContent = new String(Files.readAllBytes(passwordFile.toPath())).trim();
 			} catch(IOException ioe) {
@@ -128,11 +169,11 @@ public class ConfigureCRCFrameworksListener extends UnitedServerListener {
 		}
 		return passwordContent;
 	}
+
 	private File findAdminPasswordFile(IServer server) {
 		String home = getServerHome(server);
 		File cache = Paths.get(home, "cache").toFile();
-		File passwordFile = findFile(cache, "kubeadmin-password");
-		return passwordFile;
+		return findFile(cache, "kubeadmin-password");
 	}
 
 	private String getServerHome(IServer server) {
