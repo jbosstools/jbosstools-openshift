29696f3abb556dc7b75b98f9ad950fd92ad9215a
Stephane Bouchet <sbouchet@redhat.com>
2020-01-27 15:36:18 +0100

    JBIDE-26993: Provide download infrastructure for cli
    added test
    
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

---
+++ tests/org.jboss.tools.openshift.test/src/org/jboss/tools/openshift/test/common/ui/utils/OdoDownloadHelperTest.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.openshift.test.common.ui.utils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doReturn;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+import org.jboss.tools.common.util.DownloadHelper;
+import org.jboss.tools.openshift.internal.test.OpenShiftTestActivator;
+import org.jboss.tools.usage.event.UsageReporter;
+import org.jboss.tools.usage.internal.preferences.UsageReportPreferences;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class OdoDownloadHelperTest {
+
+	public final static String ODO_CACHE_DIR = System.getProperty("user.home") + File.separatorChar + ".odo-tests"; //$NON-NLS-1$ //$NON-NLS-2$
+
+	@BeforeClass
+	public static void setup() throws IOException {
+		FileUtils.deleteDirectory(new File(ODO_CACHE_DIR));
+		UsageReportPreferences.setEnabled(false);
+		UsageReportPreferences.setAskUser(false);
+	}
+
+	@AfterClass
+	public static void teardown() throws IOException {
+		FileUtils.deleteDirectory(new File(ODO_CACHE_DIR));
+	}
+
+	@Test
+	public void testThatODOisDownloaded() throws IOException {
+		DownloadHelper originalHelper = DownloadHelper.getInstance();
+		DownloadHelper helper = spy(originalHelper);
+		doReturn(Boolean.TRUE).when(helper).isDownloadAllowed("odo", "", "1.0.2");
+		URL url = OpenShiftTestActivator.getDefault().getBundle().getEntry("/resources/test-tools.json");
+		assertNotNull(url);
+		String cmd = helper.downloadIfRequired("odo", url);
+		assertNotNull(cmd);
+		assertEquals(ODO_CACHE_DIR + File.separatorChar + "cache" + File.separatorChar + "1.0.2" + File.separatorChar
+				+ "odo", cmd);
+		System.out.println("--------end");
+	}
+}
