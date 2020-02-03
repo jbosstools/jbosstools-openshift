033ebd5dec48c787718d45ce594143eb3f68f841
Jeff MAURY <jmaury@redhat.com>
2020-02-03 19:15:38 +0100

    Ensure expanded/collapsed state is preserved on tree refresh
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- plugins/org.jboss.tools.openshift.core/src/org/jboss/tools/openshift/core/odo/Application.java
+++ plugins/org.jboss.tools.openshift.core/src/org/jboss/tools/openshift/core/odo/Application.java
@@ -10,10 +10,43 @@
  ******************************************************************************/
 package org.jboss.tools.openshift.core.odo;
 
+import java.util.Objects;
+
 public interface Application {
   String getName();
 
   static Application of(String name) {
-    return () -> name;
+    return new ApplicationImpl(name);
+  }
+  
+  class ApplicationImpl implements Application {
+
+	private final String name;
+	
+	private ApplicationImpl(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof ApplicationImpl)) {
+			return false;
+		}
+		ApplicationImpl other = (ApplicationImpl) obj;
+		return Objects.equals(name, other.name);
+	}
   }
 }
