6f568e10540131b3b147507afb0900f1d2d5c414
Jeff MAURY <jmaury@redhat.com>
2020-02-26 15:44:24 +0100

    JBIDE-27064: Pull secret validation did not enable Finish button (#1912)
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- plugins/org.jboss.tools.openshift.cdk.server/src/org/jboss/tools/openshift/internal/crc/server/ui/CRC100ServerWizardFragment.java
+++ plugins/org.jboss.tools.openshift.cdk.server/src/org/jboss/tools/openshift/internal/crc/server/ui/CRC100ServerWizardFragment.java
@@ -154,13 +154,13 @@ public class CRC100ServerWizardFragment extends CDKServerWizardFragment {
 		String errorMessage = pullSecretValidation.validate(pullSecretFile, 
 				(String error) -> {
 					togglePullSecretDecorator(error);
-					setComplete(error != null);
+					setComplete(error == null);
 					if (error != null) {
 						handle.setMessage(error, IMessageProvider.ERROR);
 					}
 				});
 		// dont complete until job validated pull secret
-		setComplete(false);
+		setComplete(errorMessage == null);
 		togglePullSecretDecorator(errorMessage);
 		return errorMessage;
 	}
