5dc5dc404bb1610df1d51b0e37484723df084c91
Stephane Bouchet <sbouchet@redhat.com>
2020-07-08 15:40:18 +0200

    JBIDE-27367: fix "Open Console Dashboard" shows page with error
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

--- plugins/org.jboss.tools.openshift.core/src/org/jboss/tools/openshift/core/OpenShiftCoreConstants.java
+++ plugins/org.jboss.tools.openshift.core/src/org/jboss/tools/openshift/core/OpenShiftCoreConstants.java
@@ -21,4 +21,8 @@ public class OpenShiftCoreConstants {
 	public static final String OCP4_CONFIG_NAMESPACE = "openshift-config-managed";
 	public static final String OCP4_CONSOLE_PUBLIC_CONFIG_MAP_NAME = "console-public";
 	public static final String OCP4_CONSOLE_URL_KEY_NAME = "consoleURL";
+
+	public static final String OCP3_CONFIG_NAMESPACE = "openshift-web-console";
+	public static final String OCP3_WEBCONSOLE_CONFIG_MAP_NAME = "webconsole-config";
+	public static final String OCP3_WEBCONSOLE_YAML_FILE_NAME = "webconsole-config.yaml";
 }
