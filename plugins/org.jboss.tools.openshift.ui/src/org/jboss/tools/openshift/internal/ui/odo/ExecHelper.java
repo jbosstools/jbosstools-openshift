527d7a3fb2bd03d98a8c7d07a745250e6b869490
Jeff MAURY <jmaury@redhat.com>
2020-11-17 15:36:46 +0100

    JBIDE-27541: Use a better console for odo output
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/odo/ExecHelper.java
+++ plugins/org.jboss.tools.openshift.ui/src/org/jboss/tools/openshift/internal/ui/odo/ExecHelper.java
@@ -14,13 +14,20 @@ import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.exec.PumpStreamHandler;
 import org.apache.commons.io.output.WriterOutputStream;
-import org.eclipse.ui.console.ConsolePlugin;
-import org.eclipse.ui.console.IConsole;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.tm.terminal.view.core.TerminalServiceFactory;
+import org.eclipse.tm.terminal.view.core.interfaces.ITerminalService;
+import org.eclipse.tm.terminal.view.core.interfaces.constants.ITerminalsConnectorConstants;
 import static org.jboss.tools.openshift.core.OpenShiftCoreConstants.HOME_FOLDER;
 
 import java.io.File;
+import java.io.FilterInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
@@ -62,14 +69,88 @@ public class ExecHelper {
     public static String execute(String executable, boolean checkExitCode, String... arguments) throws IOException {
         return execute(executable, checkExitCode, new File(HOME_FOLDER), arguments);
     }
+    
+    private static class RedirectedStream extends FilterInputStream {
+      private boolean emitLF = false;
+      private int previous = -1;
+
+      private RedirectedStream(InputStream delegate) {
+          super(delegate);
+      }
+
+      @Override
+      public synchronized int read() throws IOException {
+          if (emitLF) {
+              emitLF = false;
+              return '\n';
+          } else {
+              int c = super.read();
+              if (c == '\n' && previous != '\r') {
+                  emitLF = true;
+                  c = '\r';
+              }
+              previous = c;
+              return c;
+          }
+      }
+
+      @Override
+      public synchronized int read(byte[] b) throws IOException {
+          return read(b, 0, b.length);
+      }
+
+      @Override
+      public synchronized int read(byte[] b, int off, int len) throws IOException {
+          if (b == null) {
+              throw new NullPointerException();
+          } else if (off < 0 || len < 0 || len > b.length - off) {
+              throw new IndexOutOfBoundsException();
+          } else if (len == 0) {
+              return 0;
+          }
+
+          int c = read();
+          if (c == -1) {
+              return -1;
+          }
+          b[off] = (byte) c;
+
+          int i = 1;
+          try {
+              for (; i < len && available() > 0; i++) {
+                  c = read();
+                  if (c == -1) {
+                      break;
+                  }
+                  b[off + i] = (byte) c;
+              }
+          } catch (IOException ee) {
+          }
+          return i;
+      }
+  }
+
 
 	private static void executeWithTerminalInternal(File workingDirectory, boolean waitForProcessToExit,
 	        String... command) throws IOException {
 		ProcessBuilder builder = new ProcessBuilder(command).directory(workingDirectory).redirectErrorStream(true);
 		Process p = builder.start();
-		IConsole odoConsole = new OdoConsole(p, command);
-		ConsolePlugin.getDefault().getConsoleManager().addConsoles(new IConsole[] { odoConsole });
-		ConsolePlugin.getDefault().getConsoleManager().showConsoleView(odoConsole);
+		InputStream in = Platform.OS_WIN32.equals(Platform.getOS())?new RedirectedStream(p.getInputStream()):p.getInputStream();
+		 InputStream err = Platform.OS_WIN32.equals(Platform.getOS())?new RedirectedStream(p.getErrorStream()):p.getErrorStream();
+		 OutputStream out = p.getOutputStream();
+		 Map<String, Object> properties = new HashMap<>();
+		 properties.put(ITerminalsConnectorConstants.PROP_DELEGATE_ID,
+		 "org.eclipse.tm.terminal.connector.streams.launcher.streams");
+		 properties.put(ITerminalsConnectorConstants.PROP_TERMINAL_CONNECTOR_ID,
+		 "org.eclipse.tm.terminal.connector.streams.StreamsConnector");
+		 properties.put(ITerminalsConnectorConstants.PROP_TITLE, String.join(" ", command));
+		 properties.put(ITerminalsConnectorConstants.PROP_LOCAL_ECHO, false);
+		 properties.put(ITerminalsConnectorConstants.PROP_FORCE_NEW, true);
+		 properties.put(ITerminalsConnectorConstants.PROP_STREAMS_STDIN, out);
+		 properties.put(ITerminalsConnectorConstants.PROP_STREAMS_STDOUT, in);
+		 properties.put(ITerminalsConnectorConstants.PROP_STREAMS_STDERR, err);
+		 ITerminalService service = TerminalServiceFactory.getService();
+		 service.openConsole(properties, null);
 		if (waitForProcessToExit) {
 			try {
 				p.waitFor();
