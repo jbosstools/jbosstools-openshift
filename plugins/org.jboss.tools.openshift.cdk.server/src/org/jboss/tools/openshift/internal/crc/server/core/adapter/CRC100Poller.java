2debc771bcfd7edfdbdd96ab06e9a9baf8a064f4
Rob Stryker <rob@oxbeef.net>
2020-11-20 12:20:42 -0500

    Fix JBIDE-27499 - use json output format for crc status (#1954)
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- plugins/org.jboss.tools.openshift.cdk.server/src/org/jboss/tools/openshift/internal/crc/server/core/adapter/CRC100Poller.java
+++ plugins/org.jboss.tools.openshift.cdk.server/src/org/jboss/tools/openshift/internal/crc/server/core/adapter/CRC100Poller.java
@@ -28,17 +28,54 @@ import org.jboss.tools.openshift.internal.cdk.server.core.adapter.AbstractCDKPol
 import org.jboss.tools.openshift.internal.cdk.server.core.adapter.OpenShiftNotReadyPollingException;
 import org.jboss.tools.openshift.internal.cdk.server.core.adapter.controllers.CDKLaunchUtility;
 import org.jboss.tools.openshift.internal.cdk.server.core.adapter.controllers.CommandTimeoutException;
+import org.jboss.tools.openshift.internal.cdk.server.core.detection.MinishiftVersionLoader;
+import org.jboss.tools.openshift.internal.cdk.server.core.detection.MinishiftVersionLoader.MinishiftVersions;
 import org.jboss.tools.openshift.internal.cdk.server.core.listeners.ServiceManagerEnvironment;
 import org.jboss.tools.openshift.internal.cdk.server.core.listeners.ServiceManagerEnvironmentLoader;
 
-public class CRC100Poller extends AbstractCDKPoller {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+public class CRC100Poller extends AbstractCDKPoller {
+	private String crcVers;
+	
 	protected void launchThread() {
 		launchThread("CodeReady Containers Poller");
 	}
 
 	protected Map<String, String> createEnvironment(IServer server) {
+		try {
+			crcVers = getCrcVersionString(server);
+		} catch (PollingException | IOException | TimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return new HashMap<>();
+	}
+	
+	protected boolean shouldUseJsonFlags() {
+		String[] versionSegments = crcVers.split("\\.");
+		int[] versionSegmentsInt = new int[versionSegments.length];
+		for( int i = 0; i < versionSegments.length; i++ ) {
+			versionSegmentsInt[i] = Integer.parseInt(versionSegments[i]);
+		}
+		return compareVersions(versionSegmentsInt, new int[] {1, 17, 0 }) >= 0;
+	}
+
+	private int compareVersions(int[] versionSegments, int[] is) {
+		if( versionSegments[0] > is[0] )
+			return 1;
+		if( versionSegments[0] < is[0] )
+			return -1;
+		if( versionSegments[1] > is[1] )
+			return 1;
+		if( versionSegments[1] < is[1] )
+			return -1;
+		if( versionSegments[2] > is[2] )
+			return 1;
+		if( versionSegments[2] < is[2] )
+			return -1;
+		return 0;
 	}
 
 	@Override
@@ -53,10 +90,25 @@ public class CRC100Poller extends AbstractCDKPoller {
 		}
 		return f;
 	}
+	
+
+	protected String getCrcVersionString(IServer server)
+			throws PollingException, IOException, TimeoutException {
+		String cmdLoc = CRC100Server.getCRCBinaryLocation(server);
+		MinishiftVersions msVers = MinishiftVersionLoader.getVersionProperties(cmdLoc);
+		return msVers.getCRCVersion();
+	}
 
 	protected IStatus onePing(IServer server, Map<String, String> env)
 			throws PollingException, IOException, TimeoutException {
-
+		if( shouldUseJsonFlags()) {
+			return runOnePingJson(server, env);
+		} else {
+			return runOnePingLegacy(server, env);
+		}
+	}
+	protected IStatus runOnePingLegacy(IServer server, Map<String, String> env)
+			throws PollingException, IOException, TimeoutException {
 		String[] args = new String[] { CDKConstants.VAGRANT_CMD_STATUS };
 		String cmdLoc = CRC100Server.getCRCBinaryLocation(server);
 		try {
@@ -69,6 +121,41 @@ public class CRC100Poller extends AbstractCDKPoller {
 			} else {
 				return stat;
 			}
+		} catch (CommandTimeoutException vte) {
+			// Try to salvage it, it could be the process never terminated but
+			// it got all the output
+			List<String> inLines = vte.getInLines();
+			if (inLines != null) {
+				String[] asArr = inLines.toArray(new String[inLines.size()]);
+				IStatus stat = parseOutput(asArr);
+				if (stat.isOK()) {
+					checkOpenShiftHealth(server, 4000);
+				} else {
+					return stat;
+				}
+			}
+			CDKCoreActivator.pluginLog().logError("Unable to successfully complete a call to vagrant status. ", vte);
+			throw vte;
+		}
+	}
+	protected IStatus runOnePingJson(IServer server, Map<String, String> env)
+			throws PollingException, IOException, TimeoutException {
+		String[] args = new String[] { "status", "-o", "json" };
+		String cmdLoc = CRC100Server.getCRCBinaryLocation(server);
+		try {
+			String[] lines = new CDKLaunchUtility().callMachineReadable(cmdLoc, args, 
+					getWorkingDirectory(server), env);
+			String allContent = String.join("\n", lines);
+		    ObjectMapper mapper = new ObjectMapper();
+		    JsonNode actualObj = mapper.readTree(allContent);
+		    JsonNode field = actualObj.get("crcStatus");
+		    String status = field.asText();
+		    if( "Running".equals(status)) 
+		    	return Status.OK_STATUS;
+		    if( "Stopped".equals(status)) 
+				return CDKCoreActivator.statusFactory()
+						.errorStatus("crc status indicates the CodeReady Container is stopped.");
+			return StatusFactory.infoStatus(CDKCoreActivator.PLUGIN_ID, "The CRC Container is starting.");
 		} catch (CommandTimeoutException vte) {
 			// Try to salvage it, it could be the process never terminated but
 			// it got all the output
