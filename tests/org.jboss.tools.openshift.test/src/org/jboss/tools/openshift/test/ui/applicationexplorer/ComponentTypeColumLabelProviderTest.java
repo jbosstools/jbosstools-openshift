fefa92acf5fcd188208760e321fd0be824840cd5
Jeff MAURY <jmaury@redhat.com>
2021-12-03 09:39:15 +0100

    JBIDE-28197: Component are displayed using their id not name (#2044)
    * JBIDE-28197: Component are displayed using their id not name
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

---
+++ tests/org.jboss.tools.openshift.test/src/org/jboss/tools/openshift/test/ui/applicationexplorer/ComponentTypeColumLabelProviderTest.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Red Hat, Inc. Distributed under license by Red Hat, Inc.
+ * All rights reserved. This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Red Hat, Inc.
+ ******************************************************************************/
+package org.jboss.tools.openshift.test.ui.applicationexplorer;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.jboss.tools.openshift.core.odo.DevfileComponentType;
+import org.jboss.tools.openshift.core.odo.DevfileRegistry;
+import org.jboss.tools.openshift.internal.ui.wizard.applicationexplorer.ComponentTypeColumLabelProvider;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class ComponentTypeColumLabelProviderTest {
+
+	private ComponentTypeColumLabelProvider provider;
+
+	@Before
+	public void setup() throws Exception {
+		provider = new ComponentTypeColumLabelProvider();
+	}
+	
+	@Test
+	public void checkDisplayNameIsUsed() {
+		DevfileRegistry registry = mock(DevfileRegistry.class);
+		when(registry.getName()).thenReturn("registry name");
+		DevfileComponentType type = new DevfileComponentType("my name", "my display name", "my description", registry, null, null);
+		String result = provider.getText(type);
+		assertTrue(result.equals("my display name (from registry name)"));
+	}
+ }
