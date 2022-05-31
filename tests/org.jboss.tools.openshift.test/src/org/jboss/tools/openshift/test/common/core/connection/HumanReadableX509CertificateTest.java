fc711d8ada17c5009a0f3e43187670f7052e4a43
Stephane Bouchet <sbouchet@redhat.com>
2022-05-31 16:12:46 +0200

    fix certificate
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

--- tests/org.jboss.tools.openshift.test/src/org/jboss/tools/openshift/test/common/core/connection/HumanReadableX509CertificateTest.java
+++ tests/org.jboss.tools.openshift.test/src/org/jboss/tools/openshift/test/common/core/connection/HumanReadableX509CertificateTest.java
@@ -57,12 +57,12 @@ public class HumanReadableX509CertificateTest {
 		String issuedTo = certificate.getIssuedTo();
 		// then
 		assertThat(getValue(LABEL_PRINCIPAL_COUNTRY, issuedTo)).isEqualTo("US");
-		assertThat(getValue(LABEL_PRINCIPAL_STATE, issuedTo)).isEqualTo("North Carolina");
+		assertThat(getValue(LABEL_PRINCIPAL_STATE, issuedTo)).isEqualTo("NC");
 		assertThat(getValue(LABEL_PRINCIPAL_LOCALITY, issuedTo)).isEqualTo("Raleigh");
-		assertThat(getValue(LABEL_PRINCIPAL_ORGANISATION, issuedTo)).isEqualTo("Red Hat, Inc.");
-		// not present in current certificate for redhat.com
-		// assertThat(getValue(LABEL_PRINCIPAL_ORGANISATIONAL_UNIT, issuedTo)).isEqualTo("IT");
-		assertThat(getValue(LABEL_PRINCIPAL_COMMON_NAME, issuedTo)).isEqualTo("www.redhat.com");
+		assertThat(getValue(LABEL_PRINCIPAL_ORGANISATION, issuedTo)).isEqualTo("Red Hat");
+		// present in current certificate for redhat.com
+		assertThat(getValue(LABEL_PRINCIPAL_ORGANISATIONAL_UNIT, issuedTo)).isEqualTo("IT");
+		assertThat(getValue(LABEL_PRINCIPAL_COMMON_NAME, issuedTo)).isEqualTo("Red Hat");
 	}
 
 	@Test
@@ -71,7 +71,7 @@ public class HumanReadableX509CertificateTest {
 		// when
 		String issuedToCommonName = certificate.getIssuedTo(HumanReadableX509Certificate.PRINCIPAL_COMMON_NAME);
 		// then
-		assertThat(issuedToCommonName).isEqualTo("www.redhat.com");
+		assertThat(issuedToCommonName).isEqualTo("Red Hat");
 	}
 
 	@Test
@@ -81,9 +81,9 @@ public class HumanReadableX509CertificateTest {
 		String issuedBy = certificate.getIssuedBy();
 		// then
 		assertThat(getValue(LABEL_PRINCIPAL_COUNTRY, issuedBy)).isEqualTo("US");
-		assertThat(getValue(LABEL_PRINCIPAL_ORGANISATION, issuedBy)).isEqualTo("DigiCert Inc");
-		assertThat(getValue(LABEL_PRINCIPAL_ORGANISATIONAL_UNIT, issuedBy)).isEqualTo("www.digicert.com");
-		assertThat(getValue(LABEL_PRINCIPAL_COMMON_NAME, issuedBy)).isEqualTo("DigiCert SHA2 Extended Validation Server CA");
+		assertThat(getValue(LABEL_PRINCIPAL_ORGANISATION, issuedBy)).isEqualTo("Red Hat");
+		assertThat(getValue(LABEL_PRINCIPAL_ORGANISATIONAL_UNIT, issuedBy)).isEqualTo("IT");
+		assertThat(getValue(LABEL_PRINCIPAL_COMMON_NAME, issuedBy)).isEqualTo("Red Hat");
 	}
 
 	@Test
@@ -92,15 +92,15 @@ public class HumanReadableX509CertificateTest {
 		// when
 		String fingerprint = certificate.getFingerprint();
 		// then
-		assertThat(fingerprint).isEqualTo("3B20918BE121B4B09AB94791D1887993D6C4DEB9");
+		assertThat(fingerprint).isEqualTo("2F09D9E93341251856A912829E219671D322B876");
 	}
 
 	@Test
 	public void shouldReportValidity() throws CertificateException, ParseException {
 		// given
 		SimpleDateFormat dateParser = new SimpleDateFormat(DATE_FORMAT, Locale.US);
-		Calendar expectedIssuedOn = getCalendar(dateParser.parse("Wed, 24 Feb 2020 01:00:00 CET"));
-		Calendar expectedExpiresOn = getCalendar(dateParser.parse("Fri, 24 May 2022 13:00:00 CET"));
+		Calendar expectedIssuedOn = getCalendar(dateParser.parse("Mon, 12 Mar 2018 18:25:23 CET"));
+		Calendar expectedExpiresOn = getCalendar(dateParser.parse("Sat, 02 Mar 2058 18:25:23 CET"));
 		// when
 		String validity = certificate.getValidity();
 		// then
